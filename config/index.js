e2469ef1b1b503a8140b42ef01dcd131d163402f
Tosh Koevoets <tosh.koevoets@gmail.com>
2019-01-04 14:26:42 +0100

    Styling fixes for logo

--- config/index.js
+++ config/index.js
@@ -55,5 +55,5 @@ exports.db = {
  */
 exports.session = {
   maxAge : 3600000 * 24 * 7 * 52,
-  secret : 'A Secret That Should Be Changed', // TODO: You need to change this secret to something that you choose for your secret
+  secret : 'jdsch9wfiubaweb1221sjasdj', // TODO: You need to change this secret to something that you choose for your secret
 };
