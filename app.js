035709f3c6b1ba79237b7dee1a3ad1416378e2ea
Tosh Koevoets <tosh.koevoets@gmail.com>
2019-08-13 11:06:51 +0200

    remove memorystore comment

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use(expressSession({
   saveUninitialized : true,
   resave            : true,
   secret            : config.session.secret,
-  store             : new MemoryStore(),
+//  store             : new MemoryStore(),
   store             : new FileStore({
     ttl: 3600 * 24 * 31
   }),
