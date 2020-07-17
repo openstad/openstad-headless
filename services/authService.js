d6ae60aeca51b2183ead936944939533e308b12c
stijnvdvegt <stijnvandervegt@gmail.com>
2020-07-17 12:48:31 +0200

    add very simple unit test and introduce userRepository

--- services/authService.js
+++ services/authService.js
@@ -1,18 +1,10 @@
-const User = require('../models/index').User;
 const ActionLog = require('../models/index').ActionLog;
+const userRepository = require('../repositories/userRepository');
 const privilegedRoles =  require('../config/roles').privilegedRoles;
 
-exports.validateUser = async (email, clientId) => {
-
+exports.validatePrivilegeUser = async (email, clientId) => {
   // Get user for specific client and assigned to one of the privilegedRoles
-  const user = await User
-    .query()
-    .join('user_roles', 'user_roles.userId', 'users.id')
-    .join('roles', 'roles.id', 'user_roles.roleId')
-    .where('users.email', '=', email)
-    .where('user_roles.clientId', '=', clientId)
-    .whereIn('roles.name', privilegedRoles)
-    .first();
+  const user = await userRepository.getUserByClientAndRoles(email, clientId, privilegedRoles);
 
   if(!user) {
     throw new Error('User not found or user does not have the allowed roles');
