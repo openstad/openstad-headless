b0e3bc416cc606b124fdd0616cc7d6d63acfece5
BartWtje <bartw.tje@hotmail.com>
2023-09-29 09:43:42 +0200

    Spacing aanpassingen op likes en keuzewijzer

--- apps/web/src/pages/projects/[project]/widgets/like/index.tsx
+++ apps/web/src/pages/projects/[project]/widgets/like/index.tsx
@@ -79,13 +79,13 @@ export default function WidgetLikes() {
                                     </FormItem>
                                 )}
                                 />
+                                <div className="sticky bottom-0 py-4 bg-background border-t border-border flex flex-col">
+                                    <Button className="self-end" type="submit">
+                                        Opslaan
+                                    </Button>
+                                </div>
                             </form>
                         </Form>
-                        <div className="sticky bottom-0 py-4 bg-background border-t border-border flex flex-col">
-                            <Button className="self-end" type="submit">
-                                Opslaan
-                            </Button>
-                        </div>
                     </div>
                 </div>
             </PageLayout>
