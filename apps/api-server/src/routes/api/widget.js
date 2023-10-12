f0ffd32da9e4c65b0f08827b0e2d721a41c6d1de
LorenzoJokhan <lorenzoluk@gmail.com>
2023-10-12 14:31:30 +0200

    Added create route for widget

--- apps/api-server/src/routes/api/widget.js
+++ apps/api-server/src/routes/api/widget.js
@@ -11,32 +11,63 @@ router.all('*', function(req, res, next) {
       req.scope.push('includeType');
     }
     return next();
+  })
+  .all(function(req, res, next) {
+    if(!req.params.projectId) return res.status(400).send("projectId is required")
   });
 
 // list widgets
 // --------------
 router.route('/')
-.get(auth.useReqUser)
-.get(function(req, res, next) {
-
-    let { dbQuery } = req;
-    dbQuery.where = {
-    projectId: req.params.projectId,
-    ...req.queryConditions,
-    }
-
-    db.Widget
-    .scope(...req.scope)
-    .findAndCountAll(dbQuery)
-    .then(function(result) {
-        const { rows } = result;
-        req.results = rows;    
-        return next();
+    .all(function(req, res, next) {
+        let { dbQuery } = req;
+        dbQuery.where = {
+            projectId: req.params.projectId,
+             ...req.queryConditions,
+        }
+        db.Widget
+        .scope(...req.scope)
+        .findAndCountAll(dbQuery)
+        .then(function(result) {
+            const { rows } = result;
+            req.results = rows;    
+            return next();
+        })
+        .catch(next);
     })
-    .catch(next);
-})
-.get(function(req, res, next) {
-    res.json(req.results);
+    
+    // list
+    .get(auth.useReqUser)
+    .get(function(req, res, next) {
+        return res.json(req.results);
+    })
+
+    // Create widget
+    .post(auth.useReqUser)
+    .post(function(req, res, next) {
+        const widget = req.body;
+        if(!widget.type) return res.status(400).send("Widget type is required");
+        if(!widget.description) return res.status(400).send("Widget description is required");
+        next();
+    })
+.post(async function(req, res, next) {
+    const widget = req.body;
+    const projectId = req.params.projectId;
+    const query = {where:{id: widget.type}}
+
+    const result = await db.WidgetType.findOne(query);
+    if(!result) {
+        return res.status(404).send("The given widget type does not exist");
+    }
+    
+    const createdWidget = await db.Widget.create({
+        projectId,
+        description: widget.description,
+        widgetTypeId: widget.type,
+        config: {}
+    });
+
+    return res.json(createdWidget);
 });
 
 // one widget routes: get widget
@@ -73,6 +104,19 @@ router.route('/:id') //(\\d+)
         if(config) {
             widget.update({config}).then(result => res.json(result))
         }
-	});
+	})
+    // delete widget
+    .delete(auth.useReqUser)
+    .put(async function(req, res, next) {
+		const widget = req.widget;
+        const config = {...widget.config, ...(req.body?.config || {})}; 
+    })
+    .delete(async function(req, res, next) {
+        const widget = req.widget;
+            
+        if(config) {
+            widget.update({config}).then(result => res.json(result))
+        }
+    });
     
 module.exports = router;
