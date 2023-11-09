26573062ab165de0de305be38fcfb3e292ea5655
LorenzoJokhan <lorenzoluk@gmail.com>
2023-11-09 15:52:17 +0100

    Dialog as component added to ui

---
+++ packages/ui/src/dropdown/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+import { PropsWithChildren } from 'react';
+import './index.css';
+
+type Props = {
+  items: Array<{ label: string; onClick: () => void }>;
+};
+
+export const DropDownMenu = ({
+  children,
+  items = [],
+}: PropsWithChildren & Props) => (
+  <DropdownMenu.Root>
+    <DropdownMenu.Trigger asChild>{children}</DropdownMenu.Trigger>
+
+    <DropdownMenu.Portal>
+      <DropdownMenu.Content className="DropdownMenuContent" sideOffset={5}>
+        {items.map((item) => (
+          <DropdownMenu.Item
+            className="DropdownMenuItem"
+            onClick={() => item.onClick()}>
+            {item.label}
+          </DropdownMenu.Item>
+        ))}
+      </DropdownMenu.Content>
+    </DropdownMenu.Portal>
+  </DropdownMenu.Root>
+);
