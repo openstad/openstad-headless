106fcaeb44a34f1b79068d961480b83bf10387eb
LorenzoJokhan <lorenzoluk@gmail.com>
2023-11-09 15:30:00 +0100

    Removed error in main.tsx from likes

--- packages/likes/src/main.tsx
+++ packages/likes/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Likes } from './likes.tsx';
+import { Likes } from './likes.js';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
