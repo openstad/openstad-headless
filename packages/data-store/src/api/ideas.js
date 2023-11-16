3c81d27b08e8f0bc1902a28099deb32fa9e937a1
LorenzoJokhan <lorenzoluk@gmail.com>
2023-11-16 15:54:10 +0100

    Replaced string with url search params

--- packages/data-store/src/api/ideas.js
+++ packages/data-store/src/api/ideas.js
@@ -1,23 +1,27 @@
 export default {
   fetch: async function ({ projectId }, data, options) {
-    let filterString = '';
+    const params = new URLSearchParams();
+
     const tags = options?.filter?.tags;
     if (tags) {
       for (let type in tags) {
         const tagValues = tags[type];
         if (tagValues)
           if (Array.isArray(tagValues)) {
-            tagValues.forEach((tag) => (filterString += `&tags=${tag}`));
+            tagValues.forEach((tag) => {
+              params.append('tags', tag);
+            });
           } else {
-            filterString += `&tags=${options.filter.tags[type]}`;
+            params.append('tags', tags[type]);
           }
       }
 
-      if (options.filter.search?.text)
-        filterString += `&search[text]=${options.filter.search.text}`;
+      if (options?.filter?.search?.text) {
+        params.append('search[text]', options.filter.search.text);
+      }
     }
 
-    let url = `/api/project/${projectId}/idea?includeUser=1&includeUserVote=1&includeVoteCount=1&includeTags=1${filterString}`;
+    let url = `/api/project/${projectId}/idea?includeUser=1&includeUserVote=1&includeVoteCount=1&includeTags=1&${params.toString()}`;
     return this.fetch(url);
   },
 };
