e114dcc3e7f861b96dc5526fa795259ad093c727
BartWtje <bartw.tje@hotmail.com>
2023-11-17 15:38:32 +0100

    Aanmaken tags gedeeltelijk opgezet

--- apps/web/src/hooks/use-tags.tsx
+++ apps/web/src/hooks/use-tags.tsx
@@ -1,9 +1,19 @@
 import useSWR from 'swr';
 
- export default function useTags(projectId?: string) {
-   const url = `/api/openstad/api/project/${projectId}/tag`;
+export default function useTags(projectId?: string) {
+  const url = `/api/openstad/api/project/${projectId}/tag`;
 
-   const tagListSwr = useSWR(projectId ? url : null);
+  const tagListSwr = useSWR(projectId ? url : null);
 
-   return {...tagListSwr}
- }
+  async function createTag(projectId: string, name: string, type: string) {
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ projectId, name, type }),
+    });
+  }
+
+  return {...tagListSwr, createTag}
+}
