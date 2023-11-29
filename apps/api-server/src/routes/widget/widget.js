301eb86624d337ba9cacd26b49ecdfea742cd5e1
Rudi van Hierden <rudi@draad.nl>
2023-11-29 10:53:03 +0100

    feat(widget-delivery): add `preview` route
    The /widget/preview route allows you to set a `Widget-Config` header containing a JSON representation of the config you want to pass to the widget. A `widgetType` matching a key in the `widgetSettingsMapping` must be provided, otherwise an error will be returned. All keys except for the `widgetType` will be passed on to the config.

--- apps/api-server/src/routes/widget/widget.js
+++ apps/api-server/src/routes/widget/widget.js
@@ -14,6 +14,46 @@ let router = express.Router({ mergeParams: true });
 // brute force
 router.use(bruteForce.globalMiddleware);
 
+// Configured route allows us to send a widget config through the `Widget-Config` header
+// This route is used to show a preview of a widget in the admin
+// The `Widget-Config` header must include a `widgetType` key, matching a widget type in the `widgetSettingsMapping` object
+// All keys except for `widgetType` will be passed to the widget as config
+router.route('/preview')
+.all((req, res, next) => {
+  const config = req.header('Widget-Config');
+  if (!config) {
+    return next(createError(401, 'No widget config provided'));
+  }
+  
+  try {
+    req.widgetConfig = JSON.parse(config);
+  } catch (e) {
+    return next(createError(401, 'Invalid widget config provided'));
+  }
+  
+  if (!req.widgetConfig.widgetType) {
+    return next(createError(401, 'No widget type provided'));
+  }
+  
+  return next();
+})
+.get((req, res, next) => {
+    const widgetId       = Math.floor(Math.random() * 1000000);
+    const randomId       = Math.floor(Math.random() * 1000000);
+    const componentId    = `osc-component-${widgetId}-${randomId}`;
+    const widgetType = req.widgetConfig.widgetType;
+    const widgetSettings = widgetSettingsMapping[widgetType];
+    
+    // Remove widgetType from config, but pass all other keys to the widget
+    delete req.widgetConfig.widgetType;
+    const widgetConfig = JSON.stringify(req.widgetConfig);
+    
+    let output = getWidgetJavascriptOutput(widgetSettings, widgetType, componentId, widgetConfig);
+    
+    res.header('Content-Type', 'application/javascript');
+    res.send(output);
+});
+
 router
   .route('/:widgetId([a-zA-Z0-9]+)')
   .all((req, res, next) => {
@@ -33,49 +73,24 @@ router
       .catch(next);
   })
   .get((req, res) => {
-    const widgetId = req.params.widgetId;
-    const randomId = Math.floor(Math.random() * 1000000);
-    const componentId = `osc-component-${widgetId}-${randomId}`;
-    const widget = req.widget;
+    const widgetId       = req.params.widgetId;
+    const randomId       = Math.floor(Math.random() * 1000000);
+    const componentId    = `osc-component-${widgetId}-${randomId}`;
+    const widget         = req.widget;
     const widgetSettings = widgetSettingsMapping[widget.type];
     
-    // If we include remix icon in the components, we are sending a lot of data to the client
-    // By using a CDN and loading it through a <link> tag, we reduce the size of the response and leverage browser cache
-    const remixIconCss = process.env.REMIX_ICON_CDN || "https://unpkg.com/remixicon@3.5.0/fonts/remixicon.css"
-
-    let output = '';
-    let widgetOutput = '';
-    let css = '';
-
-    widgetSettings.js.forEach((file) => {
-      widgetOutput += fs.readFileSync(
-        require.resolve(file),
-        'utf8'
-      );
-    });
-
-    widgetSettings.css.forEach((file) => {
-      css += fs.readFileSync(
-        require.resolve(file),
-        'utf8'
-      );
-    });
-
-    // Rewrite the url to the images that we serve statically
-    css = css.replaceAll('url(../images/', `url(${config.url}/widget/${widget.type}-images/`);
-
-    const loginUrl = `${config.url}/auth/project/${widget.project.id}/login?useAuth=default&redirectUri=[[REDIRECT_URI]]`;
+    const loginUrl  = `${config.url}/auth/project/${widget.project.id}/login?useAuth=default&redirectUri=[[REDIRECT_URI]]`;
     const logoutUrl = `${config.url}/auth/project/${widget.project.id}/logout?useAuth=default&redirectUri=[[REDIRECT_URI]]`;
-
+    
     const defaultConfig = {
       ...widget.project.config,
-      api: {
+      api:       {
         url: config.url,
       },
-      login: {
+      login:     {
         url: loginUrl,
       },
-      logout: {
+      logout:    {
         url: logoutUrl,
       },
       projectId: widget.project.id,
@@ -86,11 +101,60 @@ router
       ...defaultConfig,
       ...flattenObject(widget.config)
     });
+    
+    let output = getWidgetJavascriptOutput(widgetSettings, widget.type, componentId, widgetConfig);
+    
+    res.header('Content-Type', 'application/javascript');
+    res.send(output);
+  });
 
-    // Create function to render component
-    // The process.env.NODE_ENV is set to production, otherwise some React dependencies will not work correctly
-    // @todo: find a way around this so we don't have to provide the `process` variable
-    output += `
+// Add a static route for the images used in the CSS in each of the widgets
+Object.keys(widgetSettingsMapping).forEach((widget) => {
+  
+  if (!widgetSettingsMapping[widget].css) return;
+  
+  router.use(
+    `/${widget}-images`,
+    express.static(
+      path.resolve(
+        require.resolve(widgetSettingsMapping[widget].css[0]),
+        '../../images/'
+      )
+    )
+  );
+  
+});
+
+function getWidgetJavascriptOutput(widgetSettings, widgetType, componentId, widgetConfig) {
+  // If we include remix icon in the components, we are sending a lot of data to the client
+  // By using a CDN and loading it through a <link> tag, we reduce the size of the response and leverage browser cache
+  const remixIconCss = process.env.REMIX_ICON_CDN || "https://unpkg.com/remixicon@3.5.0/fonts/remixicon.css"
+  
+  let output       = '';
+  let widgetOutput = '';
+  let css          = '';
+  
+  widgetSettings.js.forEach((file) => {
+    widgetOutput += fs.readFileSync(
+      require.resolve(file),
+      'utf8'
+    );
+  });
+  
+  widgetSettings.css.forEach((file) => {
+    css += fs.readFileSync(
+      require.resolve(file),
+      'utf8'
+    );
+  });
+  
+  // Rewrite the url to the images that we serve statically
+  css = css.replaceAll('url(../images/', `url(${config.url}/widget/${widgetType}-images/`);
+  
+  // Create function to render component
+  // The process.env.NODE_ENV is set to production, otherwise some React dependencies will not work correctly
+  // @todo: find a way around this so we don't have to provide the `process` variable
+  output += `
     let process = { env: { NODE_ENV: 'production' } };
     (function () {
       const currentScript = document.currentScript;
@@ -117,26 +181,7 @@ router
       });
     })();
     `;
-
-    res.header('Content-Type', 'application/javascript');
-    res.send(output);
-  });
-
-// Add a static route for the images used in the CSS of the widgets
-Object.keys(widgetSettingsMapping).forEach((widget) => {
-  
-  if (!widgetSettingsMapping[widget].css) return;
-  
-  router.use(
-    `/${widget}-images`,
-    express.static(
-      path.resolve(
-        require.resolve(widgetSettingsMapping[widget].css[0]),
-        '../../images/'
-      )
-    )
-  );
-  
-});
+  return output;
+}
 
 module.exports = router;
