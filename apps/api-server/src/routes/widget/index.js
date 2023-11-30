672d6283bc876ad81ef2dc866cf7b3732849ba7a
Rudi van Hierden <rudi@draad.nl>
2023-11-30 09:05:48 +0100

    chore: todo to replace cors middleware

--- apps/api-server/src/routes/widget/index.js
+++ apps/api-server/src/routes/widget/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const cors = require('cors');
 
+// @todo: this is fine for testing purposes, but we should bring this in line with the `security-headers.js` middleware.
+// That will require websites to be allowlisted in the API server, but that's a good thing.
 router.use('/', cors(), require('./widget'));
 
 module.exports = router;
