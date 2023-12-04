50497b53f7d21a63e9311bd4b7f03e2a8030e2c8
BartWtje <bartw.tje@hotmail.com>
2023-12-04 10:05:58 +0100

    Pagina voor labels opgezet

--- apps/admin-server/src/pages/projects/[project]/settings/resource/index.tsx
+++ apps/admin-server/src/pages/projects/[project]/settings/resource/index.tsx
@@ -4,6 +4,7 @@ import { PageLayout } from '@/components/ui/page-layout';
 import { useRouter } from 'next/router';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import ProjectSettingsResourceGeneral from './general';
+import ProjectSettingsResourceLabels from './label';
 
 export default function ProjectSettingsResource() {
   const router = useRouter();
@@ -28,12 +29,16 @@ export default function ProjectSettingsResource() {
           },
         ]}>
         <div className="container py-6">
-          <Tabs defaultValue="preview">
+          <Tabs defaultValue="form">
             <TabsList className="w-full bg-white border-b-0 mb-4 rounded-md">
               <TabsTrigger value="form">Form</TabsTrigger>
+              <TabsTrigger value="label">Label instellingen</TabsTrigger>
             </TabsList>
             <TabsContent value="form" className="p-0">
               <ProjectSettingsResourceGeneral />
+            </TabsContent>
+            <TabsContent value="label" className="p-0">
+              <ProjectSettingsResourceLabels />
             </TabsContent>
           </Tabs>
         </div>
---
+++ apps/admin-server/src/pages/projects/[project]/settings/resource/label.tsx
@@ -0,0 +1,226 @@
+import * as React from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { Heading } from '@/components/ui/typography';
+import { Separator } from '@/components/ui/separator';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useProject } from '../../../../../hooks/use-project';
+
+const formSchema = z.object({
+  textOpen: z.string(),
+  textClosed: z.string(),
+  optionRejected: z.enum(['']),
+  textAccepted: z.string(),
+  optionAccepted: z.enum(['']),
+  optionDone: z.enum(['']),
+  optionConsidered: z.enum(['']),
+});
+
+export default function ProjectSettingsResourceLabels() {
+  const router = useRouter();
+  const { project } = router.query;
+  const { data, isLoading } = useProject();
+  const defaults = () => ({});
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver<any>(formSchema),
+    defaultValues: defaults(),
+  });
+
+  useEffect(() => {
+    form.reset(defaults());
+  }, [data]);
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
+  return (
+        <div className="container py-6">
+          <Form {...form} className="p-6 bg-white rounded-md">
+            <Heading size="xl">Resource labels</Heading>
+            <Separator className="my-4" />
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="lg:w-fit grid grid-cols-1 lg:grid-cols-2 gap-4">
+              <FormField
+                control={form.control}
+                name="textOpen"
+                render={({ field }) => (
+                  <FormItem className="col-span-1">
+                    <FormLabel>
+                      Tekst voor de 'Open' label
+                    </FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="textClosed"
+                render={({ field }) => (
+                  <FormItem className="col-span-1">
+                    <FormLabel>
+                      Tekst voor de 'Gesloten' label
+                    </FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="optionRejected"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      Wat gebeurt er als de 'Afgewezen' label van sprake is?
+                    </FormLabel>
+                    <Select
+                      onValueChange={(e: string) =>
+                        field.onChange(e === 'true')
+                      }
+                      value={field.value ? 'true' : 'false'}>
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Ja" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="true">Ja</SelectItem>
+                        <SelectItem value="false">Nee</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="textAccepted"
+                render={({ field }) => (
+                  <FormItem className="col-span-1">
+                    <FormLabel>
+                      Tekst voor de 'Geaccepteerd' label
+                    </FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="optionAccepted"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      Wat gebeurt er als de 'Geaccepteerd' label van sprake is?
+                    </FormLabel>
+                    <Select
+                      onValueChange={(e: string) =>
+                        field.onChange(e === 'true')
+                      }
+                      value={field.value ? 'true' : 'false'}>
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Ja" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="true">Ja</SelectItem>
+                        <SelectItem value="false">Nee</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="optionDone"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      Wat gebeurt er als de 'Afgerond' label van sprake is?
+                    </FormLabel>
+                    <Select
+                      onValueChange={(e: string) =>
+                        field.onChange(e === 'true')
+                      }
+                      value={field.value ? 'true' : 'false'}>
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Ja" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="true">Ja</SelectItem>
+                        <SelectItem value="false">Nee</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="optionConsidered"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      Wat gebeurt er als de 'In overweging' label van sprake is?
+                    </FormLabel>
+                    <Select
+                      onValueChange={(e: string) =>
+                        field.onChange(e === 'true')
+                      }
+                      value={field.value ? 'true' : 'false'}>
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Ja" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="true">Ja</SelectItem>
+                        <SelectItem value="false">Nee</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="w-fit col-span-full">
+                Opslaan
+              </Button>
+            </form>
+          </Form>
+        </div>
+  );
+}
