ca96931d385ab096f724cefe3c74b93f18e96e31
Rudi van Hierden <rudi@draad.nl>
2023-12-21 11:39:18 +0100

    fix: run correct install command

--- apps/api-server/scripts/build-packages.js
+++ apps/api-server/scripts/build-packages.js
@@ -6,6 +6,6 @@ const packages = Object.keys(widgetSettings);
 
 packages.forEach((package) => {
   console.log ('Building package: ' + package);
-  npm().cwd(`../../packages/${package}`).output(true).run('install');
+  npm().cwd(`../../packages/${package}`).output(true).install();
   npm().cwd(`../../packages/${package}`).output(true).run('build --if-present');
 });
