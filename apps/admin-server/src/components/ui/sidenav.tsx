3b91c503fb1fe667ade88c046fea9c243dcd2e72
BartWtje <bartw.tje@hotmail.com>
2024-01-08 13:44:29 +0100

    Kleine interface-gerelateerde error opgelost

--- apps/admin-server/src/components/ui/sidenav.tsx
+++ apps/admin-server/src/components/ui/sidenav.tsx
@@ -26,6 +26,7 @@ export function Sidenav({
   }, [router]);
 
   useEffect(() => {
+    console.log(data?.error)
     if (data?.error === "TokenFetchError" || data?.error === "TokenValidationFailed") {
       signOut(); // Force sign in to hopefully resolve error
     }
