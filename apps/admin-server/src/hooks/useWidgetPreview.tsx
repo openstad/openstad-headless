e703d62ee53f66f32f61dca4b382c8d77da3008b
timfeskens <TimF_1998@hotmail.com>
2024-01-11 11:42:33 +0100

    feat/add widget settings, fixed useEffect dependency array

--- apps/admin-server/src/hooks/useWidgetPreview.tsx
+++ apps/admin-server/src/hooks/useWidgetPreview.tsx
@@ -1,23 +1,31 @@
 import { useState, useEffect } from 'react';
 import { useWidgetConfig } from './use-widget-config';
 
-export function useWidgetPreview<T extends {}>(): {
+export function useWidgetPreview<T extends {}>(widgetSettings: {
+  [key: string]: any;
+}): {
   previewConfig: T | undefined;
   updatePreview: (arg: T) => void;
 } {
   const [previewConfig, setPreviewConfig] = useState<T>();
-
-  const { data: widget } = useWidgetConfig();
+  const { data: widget, isLoading: isLoadingWidget } = useWidgetConfig();
 
   // Set the preview the first time the widget config is loaded
   useEffect(() => {
-    const config = widget?.config;
-    if (config) {
-      setPreviewConfig({
-        ...config,
-      });
+    if (!previewConfig) {
+      const config = widget?.config;
+
+      if (config) {
+        setPreviewConfig({
+          ...config,
+          api: {
+            url: '/api/openstad',
+          },
+          ...widgetSettings,
+        });
+      }
     }
-  }, [widget?.config]);
+  }, [widget?.config, previewConfig, widgetSettings]);
 
   function updatePreview(config: T) {
     setPreviewConfig(config);
