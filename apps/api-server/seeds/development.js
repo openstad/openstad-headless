893679113ec2fbc7fec916bc69d1f7baa9f5b12c
Niels Vegter <niels@denes.nl>
2024-01-15 14:31:38 +0100

    Refactor resource.location from geo to json field

--- apps/api-server/seeds/development.js
+++ apps/api-server/seeds/development.js
@@ -149,7 +149,6 @@ module.exports = async function seed(config, db) {
       name: 'User two',
     });
 
-
     console.log('    10 resources');
     let resource1 = await db.Resource.create({
       userId: 2,
@@ -159,11 +158,8 @@ module.exports = async function seed(config, db) {
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque a sollicitudin velit, ac vehicula nibh. Sed nec est convallis, interdum ex id, tempus mauris. Nulla facilisi. Quisque placerat condimentum est. Sed condimentum ex a orci dignissim ultrices. Nulla aliquam placerat ornare. Nulla condimentum, risus id commodo finibus, leo mauris tincidunt purus, non viverra augue ligula et sapien. Nunc pulvinar, eros at feugiat facilisis, quam tellus elementum metus, a sodales neque nibh non magna. Integer iaculis, nisl aliquet tempor hendrerit, lacus quam sollicitudin lectus, sed consectetur eros turpis sit amet nunc. Aenean eu neque at libero eleifend fringilla. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec quis posuere lacus, vitae eleifend magna. Nunc egestas sollicitudin ipsum, maximus tincidunt leo facilisis at. In rhoncus eget libero sit amet aliquam.',
       images: [ { src: `${process.env.IMAGE_APP_URL}/image/zand.01.jpg`} ],
       location: {
-        type: 'Point',
-        coordinates: [
-          52.3710476 + ( Math.random() * .03 - .015 ),
-          4.9005494 + ( Math.random() * .03 - .015 )
-        ]
+        lat: 52.3710476 + ( Math.random() * .03 - .015 ),
+        lng: 4.9005494 + ( Math.random() * .03 - .015 )
       },
       startDate: db.sequelize.fn('now'),
       publishDate: db.sequelize.fn('now'),
@@ -177,11 +173,8 @@ module.exports = async function seed(config, db) {
       description: 'Etiam euismod odio ac augue blandit, quis ultricies ipsum ultrices. Nulla ornare odio et nisi tempor rhoncus. Maecenas et venenatis arcu. Proin efficitur neque sit amet enim suscipit laoreet. Mauris vehicula tristique lorem, eu imperdiet erat ornare eget. Quisque consectetur interdum tincidunt. Vestibulum ultricies arcu eleifend pharetra finibus. Fusce nec justo magna. Aliquam erat volutpat. Vivamus sollicitudin a sapien a auctor. Aenean id augue quis ex laoreet sodales.',
       images: [ { src: `${process.env.IMAGE_APP_URL}/image/zand.02.jpg`} ],
       location: {
-        type: 'Point',
-        coordinates: [
-          52.3710476 + ( Math.random() * .03 - .015 ),
-          4.9005494 + ( Math.random() * .03 - .015 )
-        ]
+        lat: 52.3710476 + ( Math.random() * .03 - .015 ),
+        lng: 4.9005494 + ( Math.random() * .03 - .015 )
       },
       startDate: db.sequelize.fn('now'),
       publishDate: db.sequelize.fn('now'),
@@ -195,11 +188,8 @@ module.exports = async function seed(config, db) {
       description: 'Quisque et viverra nisi. Nullam augue sapien, feugiat finibus erat a, varius congue dolor. Integer arcu urna, porta eget lacus a, imperdiet mattis ex. Curabitur eu urna eu nisl blandit scelerisque sed nec diam. Maecenas et sapien risus. Duis eget iaculis massa, et tempor tellus. Ut diam est, euismod in enim in, commodo rhoncus sapien.',
       images: [ { src: `${process.env.IMAGE_APP_URL}/image/zand.03.jpg`} ],
       location: {
-        type: 'Point',
-        coordinates: [
-          52.3710476 + ( Math.random() * .03 - .015 ),
-          4.9005494 + ( Math.random() * .03 - .015 )
-        ]
+        lat: 52.3710476 + ( Math.random() * .03 - .015 ),
+        lng: 4.9005494 + ( Math.random() * .03 - .015 )
       },
       startDate: db.sequelize.fn('now'),
       publishDate: db.sequelize.fn('now'),
@@ -213,11 +203,8 @@ module.exports = async function seed(config, db) {
       description: 'Ut eu porttitor odio. Aliquam nec volutpat diam. Etiam eget tempor lacus, sed aliquet lectus. Nullam sollicitudin urna ex, eget tempor leo ullamcorper non. Nullam dapibus et libero consequat euismod. Donec dui est, tempor ut lobortis ac, euismod quis nibh. In hac habitasse platea dictumst. Quisque malesuada ex mauris, ut aliquam lectus tristique id. Sed a ultrices arcu. Cras velit enim, euismod vel dictum eu, sodales eu tellus. In sit amet tristique purus, ac mollis turpis. Aliquam erat volutpat. Integer sit amet nisi tempus, maximus arcu a, venenatis urna.',
       images: [ { src: `${process.env.IMAGE_APP_URL}/image/zand.04.jpg`} ],
       location: {
-        type: 'Point',
-        coordinates: [
-          52.3710476 + ( Math.random() * .03 - .015 ),
-          4.9005494 + ( Math.random() * .03 - .015 )
-        ]
+        lat: 52.3710476 + ( Math.random() * .03 - .015 ),
+        lng: 4.9005494 + ( Math.random() * .03 - .015 )
       },
       startDate: db.sequelize.fn('now'),
       publishDate: db.sequelize.fn('now'),
@@ -231,11 +218,8 @@ module.exports = async function seed(config, db) {
       description: 'Pellentesque consectetur eros at tempus viverra. In hac habitasse platea dictumst. Aenean egestas urna sapien, porttitor pretium ante pharetra ut. Curabitur gravida ex sed felis vestibulum imperdiet. Pellentesque rutrum posuere tortor non placerat. Aliquam ornare massa id lacus vulputate, a rutrum sapien mattis. Proin purus nisl, lacinia in porttitor a, vulputate at diam. Sed fermentum augue in ipsum interdum, nec molestie diam placerat.',
       images: [ { src: `${process.env.IMAGE_APP_URL}/image/zand.05.jpg`} ],
       location: {
-        type: 'Point',
-        coordinates: [
-          52.3710476 + ( Math.random() * .03 - .015 ),
-          4.9005494 + ( Math.random() * .03 - .015 )
-        ]
+        lat: 52.3710476 + ( Math.random() * .03 - .015 ),
+        lng: 4.9005494 + ( Math.random() * .03 - .015 )
       },
       startDate: db.sequelize.fn('now'),
       publishDate: db.sequelize.fn('now'),
@@ -249,11 +233,8 @@ module.exports = async function seed(config, db) {
       description: 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce ornare felis id lacinia sodales. Vivamus augue ligula, ullamcorper ac volutpat non, scelerisque at neque. Nulla laoreet, sapien ac iaculis sodales, massa lacus auctor felis, consequat gravida lorem erat sed metus. Fusce lacus mauris, cursus id magna ullamcorper, lacinia eleifend mauris. Etiam scelerisque molestie dui congue eleifend. Phasellus a tortor nibh. Ut vestibulum ut risus at finibus.',
       images: [ { src: `${process.env.IMAGE_APP_URL}/image/forum.romanum.01.jpg`} ],
       location: {
-        type: 'Point',
-        coordinates: [
-          52.3710476 + ( Math.random() * .03 - .015 ),
-          4.9005494 + ( Math.random() * .03 - .015 )
-        ]
+        lat: 52.3710476 + ( Math.random() * .03 - .015 ),
+        lng: 4.9005494 + ( Math.random() * .03 - .015 )
       },
       startDate: db.sequelize.fn('now'),
       publishDate: db.sequelize.fn('now'),
@@ -267,11 +248,8 @@ module.exports = async function seed(config, db) {
       description: 'Nulla laoreet pretium tortor at placerat. Sed in est vulputate, ullamcorper nisi id, elementum enim. Nulla ac nisl id nibh auctor ullamcorper. In suscipit porttitor mi at sodales. Pellentesque diam elit, ornare in aliquam eget, dictum a libero. Donec sagittis dictum elit sit amet varius. Maecenas eget mauris ultricies, ullamcorper nibh sit amet, luctus ex. Cras rhoncus dolor dolor, at porta ligula bibendum sit amet. Proin hendrerit efficitur tortor eget consectetur. Curabitur vitae dictum velit. Praesent scelerisque, dui quis elementum sodales, nisi erat porttitor quam, quis feugiat erat augue vehicula dui. Praesent ultrices suscipit tellus, sit amet consectetur enim accumsan ac. Suspendisse ac porta velit.',
       images: [ { src: `${process.env.IMAGE_APP_URL}/image/forum.romanum.02.jpg`} ],
       location: {
-        type: 'Point',
-        coordinates: [
-          52.3710476 + ( Math.random() * .03 - .015 ),
-          4.9005494 + ( Math.random() * .03 - .015 )
-        ]
+        lat: 52.3710476 + ( Math.random() * .03 - .015 ),
+        lng: 4.9005494 + ( Math.random() * .03 - .015 )
       },
       startDate: db.sequelize.fn('now'),
       publishDate: db.sequelize.fn('now'),
@@ -285,11 +263,8 @@ module.exports = async function seed(config, db) {
       description: 'Aliquam ut magna eget ante tempor consectetur non nec nibh. Etiam id tellus eget turpis porta egestas. Sed maximus sed nisi vel ornare. Aliquam erat volutpat. Phasellus lobortis nibh in convallis eleifend. Morbi nunc nisi, consequat nec odio a, efficitur congue ipsum. Donec nec leo id urna scelerisque rutrum. Donec pulvinar nunc nisl, at dignissim purus dapibus at. Phasellus pellentesque sem turpis, et facilisis nisl tempus at.',
       images: [ { src: `${process.env.IMAGE_APP_URL}/image/forum.romanum.03.jpg`} ],
       location: {        
-        type: "Point",
-        coordinates: [
-          52.3710476 + ( Math.random() * .03 - .015 ),
-          4.9005494 + ( Math.random() * .03 - 015 )
-        ]
+        lat: 52.3710476 + ( Math.random() * .03 - .015 ),
+        lng: 4.9005494 + ( Math.random() * .03 - 015 )
       },
       startDate: db.sequelize.fn('now'),
       publishDate: db.sequelize.fn('now'),
@@ -303,11 +278,8 @@ module.exports = async function seed(config, db) {
       description: 'Etiam ultricies dui non justo laoreet convallis. Aenean odio erat, molestie et odio in, imperdiet consectetur mauris. Pellentesque lobortis lacus sodales, volutpat turpis vel, posuere purus. Aliquam non elementum dolor. Ut vel dolor nec purus hendrerit molestie. Sed tortor erat, facilisis vitae orci sit amet, maximus viverra diam. Ut non sem mollis, facilisis libero id, sollicitudin felis. Donec at orci fermentum, pretium nunc a, dictum mauris. In tristique quis sapien sed faucibus. Quisque posuere purus tortor, quis gravida mauris cursus ac. Integer semper turpis quis magna consectetur, blandit placerat tellus lobortis. Morbi commodo sem iaculis nunc ornare, vel lobortis enim lacinia. Sed vitae lacus ex.',
       images: [ { src: `${process.env.IMAGE_APP_URL}/image/forum.romanum.04.jpg`} ],
       location: {        
-        type: "Point",
-        coordinates: [
-          52.3710476 + ( Math.random() * .03 - .015 ),
-          4.9005494 + ( Math.random() * .03 - 015 )
-        ]
+        lat: 52.3710476 + ( Math.random() * .03 - .015 ),
+        lng: 4.9005494 + ( Math.random() * .03 - 015 )
       },
       startDate: db.sequelize.fn('now'),
       publishDate: db.sequelize.fn('now'),
@@ -321,11 +293,8 @@ module.exports = async function seed(config, db) {
       description: 'Nullam dignissim tincidunt urna, non vehicula enim convallis vitae. Nulla enim nibh, semper et metus quis, venenatis eleifend nisi. Phasellus sed erat est. Donec ac lobortis turpis. Nulla facilisi. Pellentesque sit amet nisi id ante maximus consequat non sit amet nisl. Sed diam metus, malesuada ac aliquet in, pulvinar ac elit. Sed feugiat a dui sit amet luctus. Morbi sit amet dignissim neque, eget blandit lorem. Suspendisse ultrices mauris felis, in fermentum metus vestibulum a. Integer congue pharetra risus a interdum. Vivamus fringilla justo ac elementum tempor. Quisque ultrices fringilla lobortis. Aliquam ullamcorper ligula eu ipsum imperdiet vestibulum. Maecenas pretium, mi eget blandit tincidunt, justo lorem ornare lorem, in vulputate diam quam mollis ligula. Etiam viverra, nisl et laoreet tristique, dui sapien volutpat leo, in euismod diam dui nec orci. ',
       images: [ { src: `${process.env.IMAGE_APP_URL}/image/forum.romanum.05.jpg`} ],
       location: {        
-        type: "Point",
-        coordinates: [
-          52.3710476 + ( Math.random() * .03 - .015 ),
-          4.9005494 + ( Math.random() * .03 - .015 )
-        ]
+        lat: 52.3710476 + ( Math.random() * .03 - .015 ),
+        lng: 4.9005494 + ( Math.random() * .03 - .015 )
       },
       startDate: db.sequelize.fn('now'),
       publishDate: db.sequelize.fn('now'),
