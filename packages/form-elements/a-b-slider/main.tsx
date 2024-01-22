6386fe654abe192914b06d8c2f0663ca75484786
iandebruin98 <ian@draad.nl>
2024-01-22 16:05:36 +0100

    feat: Create the form element a-b-slider

---
+++ packages/form-elements/a-b-slider/main.tsx
@@ -0,0 +1,81 @@
+import React, {FC, useState} from 'react';
+import { RangeSliderProps } from "./props";
+
+const RangeSlider: FC<RangeSliderProps> = ({
+    question= '',
+    description= '',
+    labelA,
+    labelB,
+    titleA,
+    titleB,
+    descriptionA,
+    descriptionB,
+    fieldRequired= false,
+    fieldKey,
+    showLabels = true,
+    minCharacters = 1,
+    maxCharacters = 100
+}) => {
+    const defaultValue = Math.ceil(maxCharacters / 2).toString();
+    const [value, setValue] = useState<string>(defaultValue);
+    const [isAnswered, setIsAnswered] = useState<boolean>(false);
+
+    return (
+        <div className="a-b-slider-container">
+            {question && (
+                <h3 className="a-b-question">{question}</h3>
+            )}
+            {description && (
+                <p>{description}</p>
+            )}
+            <div className="a-b-info-container">
+                <div className="a-b-title label-a">
+                    {showLabels && (<p className="label">A</p>)}
+                    <div className="a-b-info">
+                        <p className="title">{titleA}</p>
+                        <p className="description">{descriptionA}</p>
+                    </div>
+                </div>
+                <div className="a-b-title label-b">
+                    {showLabels && (<p className="label">B</p>)}
+                    <div className="a-b-info">
+                        <p className="title">{titleB}</p>
+                        <p className="description">{descriptionB}</p>
+                    </div>
+                </div>
+            </div>
+            <input
+                type="range"
+                min={minCharacters}
+                max={maxCharacters}
+                value={value}
+                step="1"
+                id="a-to-b-range"
+                name={fieldKey}
+                required={fieldRequired}
+                onChange={(e) => {
+                    setValue(e.target.value);
+                    if (!isAnswered) {
+                        setIsAnswered(true);
+                    }
+                }}
+                aria-label={`Selecteer een waarde tussen 1 en 100 voor ${titleA} en ${titleB}`}
+            />
+            <p id="a-b-description" className="a-b-description visually-hidden">
+                Deze slider vertegenwoordigt de waarde voor {titleA} aan de linkerkant en de waarde voor {titleB} aan de rechterkant.
+            </p>
+            <div className="a-b-label-container">
+                <p className="a-b-label label-a">
+                    {showLabels && (<span className="label">A.</span>)}
+                    <span className="label">{labelA}</span>
+                </p>
+                <p className="a-b-label label-b">
+                    {showLabels && (<span className="label">B.</span>)}
+                    <span className="label">{labelB}</span>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default RangeSlider;
