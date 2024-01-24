84748134ec1097cb57469d0455ee12e5f7d9c609
LorenzoJokhan <lorenzoluk@gmail.com>
2024-01-24 12:49:09 +0100

    Added the widget, with utility support, also enabling queryparam support to overwrite the text and date given

---
+++ packages/date-countdown-bar/src/date-countdown-bar.tsx
@@ -0,0 +1,83 @@
+import 'remixicon/fonts/remixicon.css';
+import { loadWidget } from '@openstad-headless/lib/load-widget';
+import React, { useEffect, useState } from 'react';
+import './date-countdown-bar.css';
+import { differenceInDays, parse } from 'date-fns';
+import { zonedTimeToUtc } from 'date-fns-tz';
+import { Spacer, Card } from '@openstad-headless/ui/src';
+
+export type DateDropdownBarWidgetProps = {
+  beforeText?: string;
+  afterText?: string;
+  date: string;
+};
+
+function DateDropdownBar({
+  beforeText = '',
+  date,
+  afterText = '',
+}: DateDropdownBarWidgetProps) {
+  const [daysLeft, setDaysLeft] = useState<number>(0);
+  const urlParams = new URLSearchParams(window.location.search);
+  const [dateParam] = useState<string>(urlParams.get('date') || date);
+  const [beforeTextParam] = useState<string>(urlParams.get('beforeText') || beforeText);
+  const [afterTextParam] = useState<string>(urlParams.get('afterText') || afterText);
+
+  useEffect(() => {
+    if (dateParam) {
+      try {
+        const zone = 'Europe/Berlin';
+        const parsedDate = parse(
+          dateParam,
+          'dd-mm-yyyy',
+          zonedTimeToUtc(new Date(), zone)
+        );
+
+        const givenDate = zonedTimeToUtc(parsedDate, zone);
+        givenDate.setHours(0, 0, 0, 0);
+
+        const currentDate = zonedTimeToUtc(new Date(), zone);
+        currentDate.setHours(0, 0, 0, 0);
+
+        const difference = differenceInDays(givenDate, currentDate);
+        setDaysLeft(difference < 0 ? 0 : difference);
+      } catch (e) {
+        console.error('Calculating the difference in days failed');
+      }
+    }
+  }, [dateParam]);
+
+  return (
+    <div className="osc date-countdown-bar-container">
+      {beforeTextParam.length > 0 ? (
+        <>
+          <p>{beforeTextParam}</p>
+          <Spacer />
+        </>
+      ) : null}
+
+      <>
+        {Array.from(
+          daysLeft > 10
+            ? daysLeft.toString()
+            : daysLeft.toString().padStart(2, '0')
+        ).map((nr) => (
+          <>
+          <Card>
+            <p>{nr}</p>
+          </Card>
+          </>
+        ))}
+      </>
+      {afterTextParam.length > 0 ? (
+        <>
+          <Spacer />
+          <p>{afterTextParam}</p>
+        </>
+      ) : null}
+    </div>
+  );
+}
+
+DateDropdownBar.loadWidget = loadWidget;
+export { DateDropdownBar };
---
+++ packages/date-countdown-bar/src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { DateDropdownBarWidgetProps, DateDropdownBar } from './date-countdown-bar.js';
+
+const config: DateDropdownBarWidgetProps = {
+  date: '27-01-2024',
+  beforeText: 'Het is nog niet geweest',
+  afterText:"Het is al geweest yo"
+};
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <DateDropdownBar {...config} />
+  </React.StrictMode>
+);
