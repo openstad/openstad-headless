070d3dcd05cf4d6497f4b50ef6c01bcfe3b4d1e1
Rudi van Hierden <rudi@draad.nl>
2024-01-25 13:24:58 +0100

    refactor(build-packages): use dependency tree
    This will add a dependency tree for use in the build-packages script. This also allows adds hashing for the packages folder, to prevent rebuilding the packages every time for the same files. Hash is saved in `.packages-build-hash` in the root folder.

--- apps/api-server/scripts/build-packages.js
+++ apps/api-server/scripts/build-packages.js
@@ -1,11 +1,51 @@
-const npm = require('npm-commands');
-
+const { execSync, exec } = require('child_process');
 const widgetSettings = require('../src/routes/widget/widget-settings');
+const {
+  buildPackage,
+  getDependencyPackages,
+  buildPackageByDirectory,
+} = require('./lib/build-package');
+const fs = require('fs');
+const { hashElement } = require('folder-hash');
 
-const packages = Object.keys(widgetSettings);
+async function buildAllPackages() {
+  // Calculate hash over all packages
 
-packages.forEach((package) => {
-  console.log ('Building package: ' + package);
-  npm().cwd(`../../packages/${package}`).output(true).install();
-  npm().cwd(`../../packages/${package}`).output(true).run('build --if-present');
-});
+  // Check if there was already a hash calculated, if so, and it matches current hash, skip build
+  const hashFile = '../../.packages-build-hash';
+  let oldHash = null;
+
+  if (fs.existsSync(hashFile)) {
+    oldHash = fs.readFileSync(hashFile, 'utf8');
+  }
+
+  const currentHash = await hashElement('../../packages', {
+    folders: {
+      exclude: ['.*', 'node_modules', 'dist', 'build', 'coverage', 'public'],
+    },
+  });
+
+  // If hash matches, skip build
+  if (oldHash !== null && oldHash === currentHash.hash) {
+    console.log('No changes detected, skipping build');
+    return;
+  }
+
+  // Write new hash to file
+  fs.writeFileSync(hashFile, currentHash.hash);
+
+  const packages = Object.keys(widgetSettings);
+  const dependencyPackages = await getDependencyPackages();
+
+  // Build all dependency packages
+  dependencyPackages.forEach((package) => {
+    buildPackageByDirectory(package);
+  });
+
+  // Build all non-dependency packages
+  packages.forEach((package) => {
+    buildPackage(package);
+  });
+}
+
+buildAllPackages();
