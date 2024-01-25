138c0a27597539ec0ba9c04f85a2adc64f65b997
Niels Vegter <niels@denes.nl>
2024-01-25 11:10:49 +0100

    Put API_FIXED_AUTH_KEY back in the setup

--- scripts/setup-admin.js
+++ scripts/setup-admin.js
@@ -19,6 +19,7 @@ CLIENT_SECRET=${process.env.AUTH_ADMIN_CLIENT_SECRET}
 OAUTH_URL=${process.env.AUTH_APP_URL}
 API_URL=${process.env.API_URL}
 API_URL_INTERNAL=${process.env.API_URL}
+API_FIXED_AUTH_KEY=${process.env.API_FIXED_AUTH_KEY}
 PORT=${process.env.ADMIN_PORT}
 `
     if (actions['create config']) {
