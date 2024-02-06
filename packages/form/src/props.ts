42e3a2325a8e801b6fd126d849d52fe0fe7bc856
iandebruin98 <ian@draad.nl>
2024-02-06 10:51:45 +0100

    feat: Include all the props for the possible fields

---
+++ packages/form/src/props.ts
@@ -0,0 +1,24 @@
+import { TextInputProps } from '@openstad-headless/ui/src/form-elements/text/props';
+import { TickmarkSliderProps } from "@openstad-headless/ui/src/form-elements/tickmark-slider/props.js";
+import { RangeSliderProps } from "@openstad-headless/ui/src/form-elements/a-b-slider/props.js";
+import { CheckboxFieldProps } from "@openstad-headless/ui/src/form-elements/checkbox/props.js";
+import { SelectFieldProps } from "@openstad-headless/ui/src/form-elements/select/props.js";
+import { RadioboxFieldProps } from "@openstad-headless/ui/src/form-elements/radio/props.js";
+
+export interface FormProps {
+    title?: string;
+    fields: CombinedFieldProps[];
+    submitText?: string;
+    submitHandler: (data: any) => void;
+}
+
+type CombinedFieldProps = (
+    TextInputProps |
+    TickmarkSliderProps |
+    RangeSliderProps |
+    CheckboxFieldProps |
+    SelectFieldProps |
+    RadioboxFieldProps
+);
+
+export { CombinedFieldProps as FieldProps };
