20aeca22b88250c332b363f2747bbc85002c8ee1
“freekDraad” <freek@draad.nu>
2024-03-05 15:51:22 +0100

    removed logs

--- apps/cms-server/modules/openstad-iconSection-widget/ui/src/index.js
+++ apps/cms-server/modules/openstad-iconSection-widget/ui/src/index.js
@@ -2,7 +2,6 @@ export default () => {
   apos.util.widgetPlayers['openstad-iconSection'] = {
     selector: '[data-openstad-iconSection]',
     player: function (el) {
-      console.log(el.dataset.expandablelabel);
       ApostropheWidgetsIconSection.IconSection.loadWidgetOnElement(el, { ...el.dataset });
     }
   };
