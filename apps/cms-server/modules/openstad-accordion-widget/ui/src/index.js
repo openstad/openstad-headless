46046b148a93e25dd7008b9f7edb1bf520e5c99d
“freekDraad” <freek@draad.nu>
2024-03-05 10:02:38 +0100

    feedback

--- apps/cms-server/modules/openstad-accordion-widget/ui/src/index.js
+++ apps/cms-server/modules/openstad-accordion-widget/ui/src/index.js
@@ -2,7 +2,6 @@ export default () => {
   apos.util.widgetPlayers['openstad-accordion'] = {
     selector: '[data-openstad-accordion]',
     player: function (el) {
-      console.log(el.dataset.text)
       ApostropheWidgetsAccordion.Accordion.loadWidgetOnElement(el, { ...el.dataset });
     }
   };
