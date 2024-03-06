ba9fa96d5adb6652270cd75fce4a24d1af63b0fc
“freekDraad” <freek@draad.nu>
2024-03-06 09:36:29 +0100

    Bug with single quotes

--- apps/cms-server/modules/openstad-iconSection-widget/ui/src/index.js
+++ apps/cms-server/modules/openstad-iconSection-widget/ui/src/index.js
@@ -2,6 +2,7 @@ export default () => {
   apos.util.widgetPlayers['openstad-iconSection'] = {
     selector: '[data-openstad-iconSection]',
     player: function (el) {
+      console.log(el.dataset.content)
       ApostropheWidgetsIconSection.IconSection.loadWidgetOnElement(el, { ...el.dataset });
     }
   };
