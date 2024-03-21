f1d428c0a4e0fd0bbf696bb9deaadd40abc9fb7a
“freekDraad” <freek@draad.nu>
2024-03-21 14:11:57 +0100

    added widget preview

--- apps/admin-server/src/pages/projects/[project]/widgets/resourcewithmap/[id]/index.tsx
+++ apps/admin-server/src/pages/projects/[project]/widgets/resourcewithmap/[id]/index.tsx
@@ -1,85 +1,104 @@
 import React from 'react';
 import { PageLayout } from '../../../../../../components/ui/page-layout';
-import { useRouter } from 'next/router'
-import { Tabs, TabsContent, TabsList, TabsTrigger} from '../../../../../../components/ui/tabs'
-import WidgetPreview from '@/components/widget-preview';
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from '../../../../../../components/ui/tabs';
+import { useRouter } from 'next/router';
 import { useWidgetConfig } from '@/hooks/use-widget-config';
 import { useWidgetPreview } from '@/hooks/useWidgetPreview';
 import { ResourceOverviewWithMapWidgetProps } from '@openstad-headless/resource-overview-with-map/src/resourceOverviewWithMap';
+import WidgetPreview from '@/components/widget-preview';
 import WidgetPublish from '@/components/widget-publish';
-import { WithApiUrlProps, withApiUrl } from '@/lib/server-side-props-definition';
+import {
+  WithApiUrlProps,
+  withApiUrl,
+} from '@/lib/server-side-props-definition';
 
-export const getServerSideProps = withApiUrl
+export const getServerSideProps = withApiUrl;
 
-export default function WidgetResourceWithMap({
-  apiUrl
-}: WithApiUrlProps) {
+export default function WidgetResourceOverview({ apiUrl }: WithApiUrlProps) {
   const router = useRouter();
   const id = router.query.id;
   const projectId = router.query.project as string;
 
   const { data: widget, updateConfig } = useWidgetConfig();
-  const { previewConfig, updatePreview } = useWidgetPreview<ResourceOverviewWithMapWidgetProps>({
+  const { previewConfig, updatePreview } =
+    useWidgetPreview<ResourceOverviewWithMapWidgetProps>({
+      projectId,
+    });
+
+  const totalPropPackage = {
+    ...widget?.config,
+    ...previewConfig,
+    updateConfig: (config: ResourceOverviewWithMapWidgetProps) =>
+      updateConfig({ ...widget.config, ...config }),
+
+    onFieldChanged: (key: keyof ResourceOverviewWithMapWidgetProps, value: any) => {
+      if (previewConfig) {
+        updatePreview({
+          ...previewConfig,
+          [key]: value,
+        });
+      }
+    },
     projectId,
-  });
+  };
 
   return (
     <div>
       <PageLayout
-        pageHeader='Project naam'
+        pageHeader="Project naam"
         breadcrumbs={[
           {
             name: 'Projecten',
-            url: '/projects'
+            url: '/projects',
           },
           {
             name: 'Widgets',
-            url: `/projects/${projectId}/widgets`
+            url: `/projects/${projectId}/widgets`,
           },
           {
-            name: 'Counter',
-            url: `/projects/${projectId}/widgets/resourcewithmap/${id}`
-          }
+            name: 'Resource Overview',
+            url: `/projects/${projectId}/widgets/resourceoverview/${id}`,
+          },
         ]}>
-          <div className='container py-6'>
-            <Tabs defaultValue='display'>
-              <TabsList className='w-full bg-white border-b-0 mb-4 rounded-md'>
-                <TabsTrigger value="display">Instellingen</TabsTrigger>
-                <TabsTrigger value="publish">Publiceren</TabsTrigger>
-              </TabsList>
-              <TabsContent value='display' className='p-0'>
-                {/* {previewConfig ? (
-                  <CounterDisplay
-                  {...previewConfig}
-                  updateConfig={(config) =>
-                    updateConfig({ ...widget.config, ...config })
-                  }
-                  onFieldChanged={(key, value) => {
-                    if (previewConfig) {
-                      updatePreview({
-                        ...previewConfig,
-                        [key]: value,
-                      });
-                    }
-                  }}
-                />
-                ) : null} */}
-              </TabsContent>
-              <TabsContent value='publish' className='p-0'>
-                <WidgetPublish apiUrl={apiUrl} />
-              </TabsContent>
-            </Tabs>
-            <div className='py-6 mt-6 bg-white rounded-md'>
-              {previewConfig ? (
-                <WidgetPreview
-                  type="resourcewithmap"
-                  config={previewConfig}
-                  projectId={projectId as string}
-                />
-              ) : null}
-            </div>
+        <div className="container py-6">
+          <Tabs defaultValue="general">
+            <TabsList className="w-full bg-white border-b-0 mb-4 rounded-md h-fit flex flex-wrap overflow-auto">
+              <TabsTrigger value="general">Algemeen</TabsTrigger>
+              {/* <TabsTrigger value="image">Afbeeldingen</TabsTrigger> */}
+              <TabsTrigger value="display">Display</TabsTrigger>
+              {/* <TabsTrigger value="button">Knop teksten</TabsTrigger> */}
+              <TabsTrigger value="tags">Tags</TabsTrigger>
+              <TabsTrigger value="search">Zoeken</TabsTrigger>
+              <TabsTrigger value="sorting">Sorteren</TabsTrigger>
+              <TabsTrigger value="pagination">Pagination</TabsTrigger>
+              <TabsTrigger value="include">Inclusief/exclusief</TabsTrigger>
+              <TabsTrigger value="publish">Publiceren</TabsTrigger>
+            </TabsList>
+            {previewConfig ? (
+              <>
+                <TabsContent value="publish" className="p-0">
+                  <WidgetPublish apiUrl={apiUrl} />
+                </TabsContent>
+              </>
+            ) : null}
+          </Tabs>
+
+          <div className="py-6 mt-6 bg-white rounded-md">
+            {previewConfig ? (
+              <WidgetPreview
+                type="resourcewithmap"
+                config={previewConfig}
+                projectId={projectId as string}
+              />
+            ) : null}
           </div>
-        </PageLayout>
+        </div>
+      </PageLayout>
     </div>
-  )
+  );
 }
