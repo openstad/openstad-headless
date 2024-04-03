fccc2848fdc534ca9bc8356fd2ec8813ee1159f3
Niels Vegter <niels@denes.nl>
2024-04-03 12:16:08 +0200

    Better default

--- apps/admin-server/src/lib/hasRole.ts
+++ apps/admin-server/src/lib/hasRole.ts
@@ -9,7 +9,7 @@ export default function hasRole(user:MustHaveRole, minRoles: Role | Array<Role>)
   minRoles = minRoles || 'admin'; // admin can do anything
   if (!Array.isArray(minRoles)) minRoles = [minRoles];
 
-  let userRole = user && user.role || '';
+  let userRole = user && user.role || 'all';
 
   let valid = minRoles.find( minRole => {
     return roles[userRole] && roles[userRole].indexOf(minRole) != -1
