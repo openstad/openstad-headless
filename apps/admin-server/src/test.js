1aa6f5e5196a345f6120d35ee43f0acea536101f
Niels Vegter <niels@denes.nl>
2024-04-03 12:08:07 +0200

    Remove test file

--- apps/admin-server/src/test.js
+++
@@ -1,4 +0,0 @@
-let query = 'https://admin.os20.nlsvgtr.nl/projects?openstadlogintoken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjgsImF1dGhQcm92aWRlciI6Im9wZW5zdGFkIiwiaWF0IjoxNzEwOTM4OTA5LCJleHAiOjE3MjY2NjM3MDl9.oHrcERVbcawGfxkA7gqhIn1TKOfb3CpeLzhg6pEFcP8'
-query = query.replace(/(?:\?|&)openstadlogintoken=(?:.(?!&|$))+./, '');
-
-console.log(query);
