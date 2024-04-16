f23f002d5e289520534e45f18475de5a40eb7567
“freekDraad” <freek@draad.nu>
2024-04-16 14:58:00 +0200

    build fix v2

--- apps/admin-server/src/pages/projects/[project]/areas/index.tsx
+++ apps/admin-server/src/pages/projects/[project]/areas/index.tsx
@@ -26,8 +26,7 @@ export default function ProjectAreas() {
     'name': (a: any, b: any) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()),
   };
 
-
-  const sortTable = (sortType: string, el: MouseEvent<HTMLElement>) => {
+  const sortTable = (sortType: string, el: React.MouseEvent<HTMLElement, MouseEvent>) => {
     const sortFunction = sortFunctions[sortType as keyof typeof sortFunctions];
     if (!sortFunction) {
       console.error(`Invalid sortType: ${sortType}`);
