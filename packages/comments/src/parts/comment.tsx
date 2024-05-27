1b90a5d466ca99578deb18ead95e4e7aa5b79dc9
“freekDraad” <freek@draad.nu>
2024-05-27 10:12:25 +0200

    added user role to interface

--- packages/comments/src/parts/comment.tsx
+++ packages/comments/src/parts/comment.tsx
@@ -83,7 +83,7 @@ function Comment({
       <section className="comment-item-header">
         <Heading level={4} appearance='utrecht-heading-6'  className={`reaction-name`}>
           {args.comment.user && args.comment.user.displayName}{' '}
-          {args.comment.user?.role === 'admin' ? <span className='--isAdmin'>{args.comment.user?.role}</span> : null}
+          {args.comment.user && args.comment.user.role === 'admin' ? <span className='--isAdmin'>{args.comment.user.role}</span> : null}
         </Heading>
         {canEdit() || canDelete() ? (
           <DropDownMenu
