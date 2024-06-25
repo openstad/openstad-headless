b5b8f27d6eb7be2681cc1aee89192e180af1fb69
Yorick Mackaaij <yorick1230@gmail.com>
2024-06-25 15:13:26 +0200

    Add statuses when creating a new project

--- apps/api-server/src/routes/api/project.js
+++ apps/api-server/src/routes/api/project.js
@@ -173,8 +173,32 @@ router.route('/')
 	.post(function(req, res, next) {
 		db.Project
 			.create({ emailConfig: {}, ...req.body })
-			.then(result => {
+			.then(async result => {
         req.results = result;
+        const defaultStatuses = {
+          'closed': 20,
+          'accepted': 30,
+        };
+
+        Object.entries(defaultStatuses).forEach(([name, seqnr]) => {
+          let data = {
+            name,
+            seqnr,
+            addToNewResources: true,
+            projectId: result.id,
+          };
+
+          db.Status.authorizeData(data, 'create', req.user)
+            .create(data)
+            .then((result) => {
+              console.log(`Status created: ${name}, with seqnr: ${seqnr}`);
+            })
+            .catch((error) => {
+              console.error(`Error creating status ${name}:`, error);
+              next(error);
+            });
+        });
+
 				return checkHostStatus({id: result.id});
 			})
 			.then(() => {
