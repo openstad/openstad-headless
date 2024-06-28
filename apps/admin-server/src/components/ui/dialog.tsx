5d797bb737fd58df844f079fa5281178b4e8cc19
“freekDraad” <freek@draad.nu>
2024-06-28 10:06:21 +0200

    hotfix; removed radix dialog overlay. Created custom overlay

--- apps/admin-server/src/components/ui/dialog.tsx
+++ apps/admin-server/src/components/ui/dialog.tsx
@@ -18,7 +18,6 @@ const DialogOverlay = React.forwardRef<
     ref={ref}
     className={cn(
       'fixed inset-0 z-50 bg-background/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
-      className
     )}
     {...props}
   />
@@ -30,7 +29,7 @@ const DialogContent = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
 >(({ className, children, ...props }, ref) => (
   <DialogPortal>
-    <DialogOverlay />
+    <div className={'fixed inset-0 z-50 bg-background/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0'}></div>
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
