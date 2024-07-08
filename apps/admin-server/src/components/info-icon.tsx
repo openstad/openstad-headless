c0b75c5adc0d03d293b79e477804c0ba6156bfcd
“freekDraad” <freek@draad.nu>
2024-07-08 08:16:24 +0200

    iconField with class

--- apps/admin-server/src/components/info-icon.tsx
+++ apps/admin-server/src/components/info-icon.tsx
@@ -1,23 +1,8 @@
 import React from 'react';
 
 const InfoIcon = () => {
-    const iconStyle = {
-        width: '15px',
-        height: '15px',
-        borderRadius: '50%',
-        backgroundColor: '#1671ef',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: '10px',
-        color: '#fff',
-        margin: '0 0 0 8px',
-        fontFamily: 'serif',
-        lineHeight: '1'
-    };
-
     return (
-        <div style={iconStyle}>
+        <div className='infoIconStyle'>
             <span>i</span>
         </div>
     );
