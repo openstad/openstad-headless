774ee38e4e049e460077f535ebfe036ae492306e
iandebruin98 <ian@draad.nl>
2024-08-08 16:55:55 +0200

    test: Add logs for the resource notification

--- apps/api-server/src/notifications/send-engines/email.js
+++ apps/api-server/src/notifications/send-engines/email.js
@@ -1,20 +1,8 @@
-const config = require('config');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 module.exports = async function sendMessage({ message }) {
   try {
-    console.log('Creating transporter with the following configuration:');
-    console.log({
-      host: process.env.MAIL_TRANSPORT_SMTP_HOST,
-      port: process.env.MAIL_TRANSPORT_SMTP_PORT,
-      secure: process.env.MAIL_TRANSPORT_SMTP_REQUIRESSL === 'true',
-      auth: {
-        user: process.env.MAIL_TRANSPORT_SMTP_AUTH_USER,
-        // Do not log the password for security reasons
-      }
-    });
-
     const transporter = nodemailer.createTransport({
       host: process.env.MAIL_TRANSPORT_SMTP_HOST,
       port: process.env.MAIL_TRANSPORT_SMTP_PORT,
@@ -25,28 +13,15 @@ module.exports = async function sendMessage({ message }) {
       }
     });
 
-    console.log('Sending email with the following message:');
-    console.log({
+    await transporter.sendMail({
       from: message.from,
       to: message.to,
       subject: message.subject,
       text: message.text,
       html: message.body,
     });
-
-    let result = await transporter.sendMail({
-      from: message.from,
-      to: message.to,
-      subject: message.subject,
-      text: message.text,
-      html: message.body,
-    });
-
-    console.log('Email sent successfully:');
-    console.log(result);
 
   } catch (err) {
-    console.error('Error occurred while sending email:');
     console.error(err);
     throw err;
   }
