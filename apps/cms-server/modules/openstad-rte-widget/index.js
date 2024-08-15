9d958af84faebc3fe65f81ee9a0c6bb86216e90b
iandebruin98 <ian@draad.nl>
2024-08-15 15:39:12 +0200

    fix: Revert RTE changes. Only keep the link styling

--- apps/cms-server/modules/openstad-rte-widget/index.js
+++ apps/cms-server/modules/openstad-rte-widget/index.js
@@ -1,43 +1,40 @@
-// TODO: Onderstaande code zou ervoor moeten zorgen dat de toolbar in de rich-text editor werkt, maar dat doet het niet.
-//  Door de code uit te zetten werkt de toolbar wel in de modal. Het lijkt mij wel wenselijk dat je ook buiten de modal de toolbar kan gebruiken.
-
-// const contentWidgets = {
-//   '@apostrophecms/rich-text': {
-//     toolbar: [
-//       'styles',
-//       '|',
-//       'bold',
-//       'italic',
-//       'strike',
-//       'link',
-//       '|',
-//       'bulletList',
-//       'orderedList'
-//     ],
-//     styles: [
-//       {
-//         tag: 'p',
-//         label: 'Paragraph (P)',
-//       },
-//       {
-//         tag: 'h1',
-//         label: 'Heading 1 (H1)',
-//       },
-//       {
-//         tag: 'h2',
-//         label: 'Heading 2 (H2)',
-//       },
-//       {
-//         tag: 'h3',
-//         label: 'Heading 3 (H3)',
-//       },
-//       {
-//         tag: 'h4',
-//         label: 'Heading 4 (H4)',
-//       }
-//     ]
-//   }
-// };
+const contentWidgets = {
+  '@apostrophecms/rich-text': {
+    toolbar: [
+      'styles',
+      '|',
+      'bold',
+      'italic',
+      'strike',
+      'link',
+      '|',
+      'bulletList',
+      'orderedList'
+    ],
+    styles: [
+      {
+        tag: 'p',
+        label: 'Paragraph (P)',
+      },
+      {
+        tag: 'h1',
+        label: 'Heading 1 (H1)',
+      },
+      {
+        tag: 'h2',
+        label: 'Heading 2 (H2)',
+      },
+      {
+        tag: 'h3',
+        label: 'Heading 3 (H3)',
+      },
+      {
+        tag: 'h4',
+        label: 'Heading 4 (H4)',
+      }
+    ]
+  }
+};
 
 module.exports = {
   extend: '@apostrophecms/widget-type',
@@ -49,13 +46,7 @@ module.exports = {
       text: {
         type: 'area',
         options: {
-
-          // TODO: De todo van boven heeft deze code nodig
-          // widgets: contentWidgets,
-
-          widgets: {
-            '@apostrophecms/rich-text': {}
-          }
+          widgets: contentWidgets,
         },
         max: 1
       },
