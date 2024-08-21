75fc94c1da510f9311024df6069384c01c235ac3
Rudi van Hierden <rudi@draad.nl>
2024-08-21 14:34:27 +0200

    feat: add --only-if-empty flag to `init-database` scripts

--- apps/auth-server/scripts/init-database.js
+++ apps/auth-server/scripts/init-database.js
@@ -19,6 +19,24 @@ const umzug = new Umzug({
 
 (async () => {
 
+  const args = process.argv.slice(2);
+  
+  if (args.length > 0 && args[0] == '--only-if-empty') {
+    try {
+      // Check if there are tables in the database
+      const tables = await db.sequelize.query('SHOW TABLES');
+
+      if (tables && tables[0].length > 0) {
+        console.log ('--only-if-empty was given, but database is not empty, skipping initialization');
+        process.exit(0);
+      }
+      
+    } catch (err) {
+      console.log ('--only-if-empty was given, but we encountered an error while checking database, skipping initialization');
+      process.exit(0);
+    }
+  }
+  
   try {
 
     console.log('Create database...');
