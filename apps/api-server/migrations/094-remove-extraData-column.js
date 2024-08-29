091adafac91c7b8ae985f2a78997cb49b3a4fe66
iandebruin98 <ian@draad.nl>
2024-08-29 10:01:04 +0200

    Fix migrations

--- apps/api-server/migrations/094-remove-extraData-column.js
+++ apps/api-server/migrations/094-remove-extraData-column.js
@@ -2,11 +2,11 @@ const { Sequelize } = require('sequelize');
 
 module.exports = {
   async up({ context: queryInterface }) {
-    await queryInterface.removeColumn('choice_guide_results', 'extraData');
+    await queryInterface.removeColumn('choices_guide_results', 'extraData');
   },
 
   async down({ context: queryInterface }) {
-    await queryInterface.addColumn('choice_guide_results', 'extraData', {
+    await queryInterface.addColumn('choices_guide_results', 'extraData', {
       type: Sequelize.JSON,
       allowNull: true,
     });
