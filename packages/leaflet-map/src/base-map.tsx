b16d4f57f395a99eed1562316e6e5a6aaec998ef
iandebruin98 <ian@draad.nl>
2024-10-15 15:58:16 +0200

    feat: Add support for geoJson

--- packages/leaflet-map/src/base-map.tsx
+++ packages/leaflet-map/src/base-map.tsx
@@ -15,6 +15,72 @@ import MarkerClusterGroup from './marker-cluster-group';
 import parseLocation from './lib/parse-location';
 import type { BaseMapWidgetProps } from './types/basemap-widget-props'
 // ToDo: import { searchAddressByLatLng, suggestAddresses, LookupLatLngByAddressId } from './lib/search.js';
+
+function isRdCoordinates(x: number, y: number) {
+  return x > 0 && x < 300000 && y > 300000 && y < 620000; // Typische ranges voor RD-coördinaten
+}
+
+// RD naar WGS84 (lat, lon) conversie
+const rdToWgs84 = (x: number, y: number) => {
+  if (x < 1000) x *= 1000;
+  if (y < 1000) y *= 1000;
+
+  const x0 = 155000.000;
+  const y0 = 463000.000;
+
+  const f0 = 52.156160556;
+  const l0 = 5.387638889;
+
+  const a01 = 3236.0331637;
+  const b10 = 5261.3028966;
+  const a20 = -32.5915821;
+  const b11 = 105.9780241;
+  const a02 = -0.2472814;
+  const b12 = 2.4576469;
+  const a21 = -0.8501341;
+  const b30 = -0.8192156;
+  const a03 = -0.0655238;
+  const b31 = -0.0560092;
+  const a22 = -0.0171137;
+  const b13 = 0.0560089;
+  const a40 = 0.0052771;
+  const b32 = -0.0025614;
+  const a23 = -0.0003859;
+  const b14 = 0.0012770;
+  const a41 = 0.0003314;
+  const b50 = 0.0002574;
+  const a04 = 0.0000371;
+  const b33 = -0.0000973;
+  const a42 = 0.0000143;
+  const b51 = 0.0000293;
+  const a24 = -0.0000090;
+  const b15 = 0.0000291;
+
+  const dx = (x - x0) * Math.pow(10, -5);
+  const dy = (y - y0) * Math.pow(10, -5);
+
+  let df = a01 * dy + a20 * Math.pow(dx, 2) + a02 * Math.pow(dy, 2) + a21 * Math.pow(dx, 2) * dy + a03 * Math.pow(dy, 3);
+  df += a40 * Math.pow(dx, 4) + a22 * Math.pow(dx, 2) * Math.pow(dy, 2) + a04 * Math.pow(dy, 4) + a41 * Math.pow(dx, 4) * dy;
+  df += a23 * Math.pow(dx, 2) * Math.pow(dy, 3) + a42 * Math.pow(dx, 4) * Math.pow(dy, 2) + a24 * Math.pow(dx, 2) * Math.pow(dy, 4);
+
+  const f = f0 + df / 3600;
+
+  let dl = b10 * dx + b11 * dx * dy + b30 * Math.pow(dx, 3) + b12 * dx * Math.pow(dy, 2) + b31 * Math.pow(dx, 3) * dy;
+  dl += b13 * dx * Math.pow(dy, 3) + b50 * Math.pow(dx, 5) + b32 * Math.pow(dx, 3) * Math.pow(dy, 2) + b14 * dx * Math.pow(dy, 4);
+  dl += b51 * Math.pow(dx, 5) * dy + b33 * Math.pow(dx, 3) * Math.pow(dy, 3) + b15 * dx * Math.pow(dy, 5);
+
+  const l = l0 + dl / 3600;
+
+  const fWgs = f + (-96.862 - 11.714 * (f - 52) - 0.125 * (l - 5)) / 100000;
+  const lWgs = l + (-37.902 + 0.329 * (f - 52) - 14.667 * (l - 5)) / 100000;
+
+  return {
+    lat: fWgs,
+    lon: lWgs
+  }
+};
+
+
 import 'leaflet/dist/leaflet.css';
 import './css/base-map.css';
 import type { MarkerProps } from './types/marker-props';
@@ -168,6 +234,7 @@ const BaseMap = ({
     if (mapDataLayers.length > 0) {
       mapDataLayers?.forEach((dataLayer: any) => {
         const records = dataLayer?.layer?.result?.records;
+        const geoJsonFeatures = dataLayer?.layer?.features;
 
         if (records && Array.isArray(records)) {
           records.forEach((record) => {
@@ -184,6 +251,47 @@ const BaseMap = ({
                   lng: long,
                   title: titel,
                   description: inhoud,
+                  markerId: `${parseInt((Math.random() * 1e8).toString())}`,
+                  isVisible: true,
+                  isClustered: true,
+                };
+
+                markerData.icon = L.icon({
+                  iconUrl: icon,
+                  iconSize: [30, 40],
+                  iconAnchor: [15, 40],
+                  className: 'custom-image-icon',
+                });
+
+                result.push(markerData);
+              }
+            }
+          });
+        }
+
+        if (geoJsonFeatures && Array.isArray(geoJsonFeatures)) {
+          geoJsonFeatures.forEach((feature) => {
+            const coordinates = feature.geometry?.coordinates;
+            let lat = coordinates && coordinates[1];
+            let long = coordinates && coordinates[0];
+            const {Objectnaam, Locatieaanduiding} = feature.properties;
+
+            if (isRdCoordinates(long, lat)) {
+              const converted = rdToWgs84(long, lat);
+              lat = converted.lat;
+              long = converted.lon;
+            }
+
+            if (lat && long) {
+              let icon = dataLayer?.icon;
+              icon = (!!icon && icon.length > 0) ? icon[0].url : undefined;
+
+              if (icon) {
+                let markerData: MarkerProps = {
+                  lat,
+                  lng: long,
+                  title: Objectnaam,
+                  description: Locatieaanduiding,
                   markerId: `${parseInt((Math.random() * 1e8).toString())}`,
                   isVisible: true,
                   isClustered: true,
