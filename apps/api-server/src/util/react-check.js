1da51c7f894812d96febc8ac5bbced4ac856e572
Rudi van Hierden <rudi@draad.nl>
2024-10-21 09:15:17 +0200

    fix(react-check): check if event was fired

--- apps/api-server/src/util/react-check.js
+++ apps/api-server/src/util/react-check.js
@@ -5,101 +5,84 @@ const reactDomJs =
   process.env.REACT_DOM_CDN ||
   'https://unpkg.com/react-dom@{VERSION}/umd/react-dom.production.min.js';
 
-module.exports =`
-    function loadScript(src, async = true, defer = true) {
-        return new Promise((resolve, reject) => {
-            const script = document.createElement('script');
-            script.src = src;
-            script.async = async;
-            script.defer = defer;
-            script.onload = resolve;
-            script.onerror = reject;
-            document.body.appendChild(script);
-        });
-    }
-
-    function triggerEvent(event) {
-        console.log('Triggering event:', event);
-        document.dispatchEvent(new CustomEvent(event));
-    }
-
-    function checkReactDom() {
-        console.log('Checking if ReactDOM is loaded...');
-        const hasReactDom = typeof ReactDOM !== 'undefined';
+module.exports = `
+  function triggerEvent(event) {
+    document.dispatchEvent(new CustomEvent(event));
+  }
+  
+  function openStadReactDomLoadedEventHasFired() {
+    window.OpenStadReactDomLoadedEventHasFired = true;
+  }
+  
+  document.addEventListener('OpenStadReactDomLoaded', openStadReactDomLoadedEventHasFired);
+  
+  function checkReactDom() {
+    if (!hasReactDom && !window.OpenStadReactDOMLoaded) {
     
-        if (!hasReactDom && !window.OpenStadReactDOMLoaded) {
-            console.log('ReactDOM is not loaded, loading ReactDOM...');
-            const reactVersion = React.version;
-            console.log('Detected React version:', reactVersion);
-    
-            const reactDomUrl = 'https://unpkg.com/react-dom@' + reactVersion + '/umd/react-dom.production.min.js';
-            return loadScript(reactDomUrl).then(() => {
-                console.log('ReactDOM loaded successfully.');
-                if (typeof window.createRoot === 'undefined' && typeof ReactDOM !== 'undefined' && typeof ReactDOM.createRoot !== 'undefined') {
-                    window.createRoot = ReactDOM.createRoot;
-                    console.log('createRoot function assigned to window.');
-                }
-                window.OpenStadReactDOMLoaded = true;
-                triggerEvent('OpenStadReactDomLoaded');
-            });
-        } else if (hasReactDom && ReactDOM.version.substr(0, 2) !== '18') {
-            console.error('Detected ReactDOM version:', ReactDOM.version);
-            throw new Error('ReactDOM version 18 is required');
-        } else {
-            console.log('ReactDOM already loaded.');
-            window.OpenStadReactDOMLoaded = true;
-            return Promise.resolve();
+      let reactVersion = React.version;
+      
+      // Get same version of react-dom as react, ensuring we have a xx.x.x format
+      if (!/18\.\d{1,2}\.\d{1,2}/.test(reactVersion) === false) {
+        throw new Error('React version 18 is required');
+      }
+      
+      const reactDomUrl = '${reactDomJs}'.replace('{VERSION}', reactVersion);
+      
+      const script = document.createElement('script');
+      script.src = reactDomUrl;
+      script.onload = function() {
+        if (typeof window.createRoot === 'undefined' && typeof ReactDOM !== 'undefined' && typeof ReactDOM.createRoot !== 'undefined') {
+          window.createRoot = ReactDOM.createRoot;
         }
-    }
-    
-    if (typeof React === 'undefined') {
-        // React is not yet loaded, load it dynamically
-        const reactJsUrl = 'https://unpkg.com/react@18.3.1/umd/react.production.min.js';
-        loadScript(reactJsUrl).then(() => {
-            console.log('React loaded');
-            checkReactDom();
-        });
+        document.addEventListener('OpenStadReactDomLoaded', renderWidget);
+        triggerEvent('OpenStadReactDomLoaded');
+      }
+      document.body.appendChild(script);
+      window.OpenStadReactDOMLoaded = true;
+    } else if (
+        typeof ReactDOM !== 'undefined' &&
+        ReactDOM.version.substr(0, 2) !== '18'
+    ) {
+      throw new Error('ReactDOM version 18 is required');
     } else {
-        // React is already loaded, proceed with ReactDOM
-        checkReactDom();
+      window.OpenStadReactDOMLoaded = true;
+      document.addEventListener('OpenStadReactDomLoaded', renderWidget);
+      triggerEvent('OpenStadReactDomLoaded');
+    }
+  }
+
+  const hasReact = typeof React !== 'undefined';
+  const hasReactWithScheduler = hasReact && typeof React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED !== 'undefined' && typeof React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.Scheduler !== 'undefined' && typeof React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.Scheduler.unstable_scheduleCallback !== 'undefined';
+  const hasReactDom = typeof ReactDOM !== 'undefined';
+  
+  if (!hasReact && !window.OpenStadReactLoaded) {
+    const script = document.createElement('script');
+    script.src = '${reactJs}';
+    script.onload = (e) => {
+      checkReactDom();
     }
     
-    // Main function to initialize widgets
-    function initializeWidgets() {
-        if (typeof React === 'undefined') {
-            // React is not yet loaded, load it dynamically
-            const reactJsUrl = 'https://unpkg.com/react@18.3.1/umd/react.production.min.js';
-            loadScript(reactJsUrl)
-                .then(() => {
-                    console.log('React loaded');
-                    return checkReactDom();  // Check and load ReactDOM
-                })
-                .then(() => {
-                    document.addEventListener('OpenStadReactDomLoaded', renderWidget);
-                    triggerEvent('OpenStadReactDomLoaded');
-                    console.log('Widgets initialized successfully.');
-                })
-                .catch(err => {
-                    console.error('Error loading React or ReactDOM:', err);
-                });
-        } else {
-            // React is already loaded, just check ReactDOM
-            checkReactDom()
-                .then(() => {
-                    document.addEventListener('OpenStadReactDomLoaded', renderWidget);
-                    triggerEvent('OpenStadReactDomLoaded');
-                    console.log('Widgets initialized successfully.');
-                })
-                .catch(err => {
-                    console.error('Error loading React or ReactDOM:', err);
-                });
-        }
+    document.body.appendChild(script);
+    window.OpenStadReactLoaded = true;
+  } else if (hasReact && React.version.substr(0, 2) < '18') {
+    throw new Error('React version 18 is required');
+  } else if (hasReact && !hasReactWithScheduler && !window.OpenStadReactLoaded) {
+    console.log ('Loading React 18.3.1 UMD version -- current version is without Scheduler which means React DOM won\\'t load correctly.');
+    
+    const script = document.createElement('script');
+    script.src = '${reactJs}';
+    script.onload = (e) => {
+      checkReactDom();
     }
     
-    // Ensure the initialization runs only after the DOM is ready
-    if (document.readyState === 'complete' || document.readyState === 'interactive') {
-        initializeWidgets();
+    document.body.appendChild(script);
+    window.OpenStadReactLoaded = true;
+  } else {
+    if (typeof window.OpenStadReactDomLoadedEventHasFired === 'undefined' || !window.OpenStadReactDomLoadedEventHasFired) {
+      // React has been loaded by a previous component on the page, render the widget when ReactDOM is loaded
+      document.addEventListener('OpenStadReactDomLoaded', renderWidget);
     } else {
-        document.addEventListener('DOMContentLoaded', initializeWidgets);
+      renderWidget();
     }
+  }
 `;
