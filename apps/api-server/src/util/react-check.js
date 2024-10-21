383ba47e3fe898751ad42c82efcc7d8efa064d37
Rudi van Hierden <rudi@draad.nl>
2024-10-21 14:58:55 +0200

    fix: react-check add checks and console logs

--- apps/api-server/src/util/react-check.js
+++ apps/api-server/src/util/react-check.js
@@ -18,11 +18,20 @@ module.exports = `
   
   function checkReactDom() {
     if (!hasReactDom && !window.OpenStadReactDOMLoaded) {
+    console.log ('!hasReactDom && !window.OpenStadReactDOMLoaded');
+      if (window.OpenStadReactDOMIsLoading) {
+      console.log ('window.OpenStadReactDOMIsLoading');
+        document.addEventListener('OpenStadReactDomLoaded', renderWidget);
+        return;
+      }
+      
+      window.OpenStadReactDOMIsLoading = true;
     
       let reactVersion = React.version;
       
       // Get same version of react-dom as react, ensuring we have a xx.x.x format
       if (!/18\.\d{1,2}\.\d{1,2}/.test(reactVersion) === false) {
+      console.log('react version is not 18');
         throw new Error('React version 18 is required');
       }
       
@@ -36,6 +45,7 @@ module.exports = `
         }
         document.addEventListener('OpenStadReactDomLoaded', renderWidget);
         triggerEvent('OpenStadReactDomLoaded');
+        console.log ('react dom on load');
       }
       document.body.appendChild(script);
       window.OpenStadReactDOMLoaded = true;
@@ -43,11 +53,19 @@ module.exports = `
         typeof ReactDOM !== 'undefined' &&
         ReactDOM.version.substr(0, 2) !== '18'
     ) {
+    console.log ('react dom not version 18');
       throw new Error('ReactDOM version 18 is required');
     } else {
+    console.log ('react dom check else');
       window.OpenStadReactDOMLoaded = true;
-      document.addEventListener('OpenStadReactDomLoaded', renderWidget);
-      triggerEvent('OpenStadReactDomLoaded');
+      
+      if (typeof window.OpenStadReactDomLoadedEventHasFired === 'undefined' || !window.OpenStadReactDomLoadedEventHasFired) {
+      console.log ('typeof window.OpenStadReactDomLoadedEventHasFired === 'undefined' || !window.OpenStadReactDomLoadedEventHasFired');
+        document.addEventListener('OpenStadReactDomLoaded', renderWidget);
+      } else {
+      console.log ('else van typeof window.OpenStadReactDomLoadedEventHasFired === 'undefined' || !window.OpenStadReactDomLoadedEventHasFired');
+        renderWidget();
+      }
     }
   }
 
@@ -56,33 +74,40 @@ module.exports = `
   const hasReactDom = typeof ReactDOM !== 'undefined';
   
   if (!hasReact && !window.OpenStadReactLoaded) {
+  console.log ('!hasReact && !window.OpenStadReactLoaded');
     const script = document.createElement('script');
     script.src = '${reactJs}';
     script.onload = (e) => {
       checkReactDom();
+      console.log ('react js onload');
     }
     
     document.body.appendChild(script);
     window.OpenStadReactLoaded = true;
   } else if (hasReact && React.version.substr(0, 2) < '18') {
+  console.log ('react version is not 18');
     throw new Error('React version 18 is required');
   } else if (hasReact && !hasReactWithScheduler && !window.OpenStadReactLoaded) {
+  console.log ('hasReact && !hasReactWithScheduler && !window.OpenStadReactLoaded');
     console.log ('Loading React 18.3.1 UMD version -- current version is without Scheduler which means React DOM won\\'t load correctly.');
     
     const script = document.createElement('script');
     script.src = '${reactJs}';
     script.onload = (e) => {
+      console.log ('react js onload');
       checkReactDom();
     }
     
     document.body.appendChild(script);
     window.OpenStadReactLoaded = true;
   } else {
+  console.log ('react check else');
     if (typeof window.OpenStadReactDomLoadedEventHasFired === 'undefined' || !window.OpenStadReactDomLoadedEventHasFired) {
+    console.log ('typeof window.OpenStadReactDomLoadedEventHasFired === 'undefined' || !window.OpenStadReactDomLoadedEventHasFired');
       // React has been loaded by a previous component on the page, render the widget when ReactDOM is loaded
-      document.addEventListener('OpenStadReactDomLoaded', renderWidget);
       checkReactDom();
     } else {
+    console.log ('else van typeof window.OpenStadReactDomLoadedEventHasFired === 'undefined' || !window.OpenStadReactDomLoadedEventHasFired');
       renderWidget();
     }
   }
