8fee78ca7674a723f16b5581bcc8bce0d40fef38
iandebruin98 <ian@draad.nl>
2024-11-06 14:42:07 +0100

    feat: Show delete button only if user can delete, hide login button otherwise

--- packages/resource-overview/src/resource-overview.tsx
+++ packages/resource-overview/src/resource-overview.tsx
@@ -479,7 +479,6 @@ function ResourceOverview({
   }, [filteredResources]);
 
   const { data: currentUser } = datastore.useCurrentUser({ ...props });
-  const isModerator = hasRole(currentUser, 'moderator');
 
   const onResourceClick = useCallback(
     (resource: any, index: number) => {
@@ -532,7 +531,7 @@ function ResourceOverview({
             itemRenderer={(item) => (
               <GridderResourceDetail
                 resource={item}
-                isModerator={isModerator}
+                currentUser={currentUser}
                 loginUrl={props.login?.url}
                 displayDocuments={displayDocuments}
                 documentsTitle={documentsTitle}
