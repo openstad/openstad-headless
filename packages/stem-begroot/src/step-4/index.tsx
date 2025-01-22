d017e7e28cc7c6ae0aec5ab53d2654911d6983c3
“freekDraad” <freek@draad.nu>
2025-01-22 11:16:32 +0100

    changed location of newsletter button

--- packages/stem-begroot/src/step-4/index.tsx
+++ packages/stem-begroot/src/step-4/index.tsx
@@ -6,33 +6,16 @@ type Props = {
   loginUrl: string;
   voteMessage: string;
   thankMessage: string;
-  showNewsletterButton: boolean;
-  newsLetterTitle: string;
 };
 export const Step4 = ({
-  showNewsletterButton,
   thankMessage,
   voteMessage,
-  newsLetterTitle
 }: Props) => {
   return (
     <>
       <Spacer size={1.5} />
       <Heading5>{voteMessage}</Heading5>
       <Paragraph>{thankMessage}</Paragraph>
-
-      {/*{showNewsletterButton ? (*/}
-      {/*  <>*/}
-      {/*    <Spacer size={2} />*/}
-      {/*    <Button*/}
-      {/*      appearance='primary-action-button'*/}
-      {/*      onClick={() => {*/}
-      {/*        // What should happen here?*/}
-      {/*      }}>*/}
-      {/*      {newsLetterTitle}*/}
-      {/*    </Button>*/}
-      {/*  </>*/}
-      {/*) : null}*/}
     </>
   );
 };
