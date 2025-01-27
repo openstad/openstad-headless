4011b979376bccde8de2b83e2d38b38631b18505
IanR01 <ian@rieken.me>
2025-01-27 16:31:33 +0100

    feat(auth): add azure-auth-token as new database authentication method

--- apps/auth-server/utils/getDbPassword.js
+++ apps/auth-server/utils/getDbPassword.js
@@ -1,5 +1,8 @@
 const getDbPassword = async () => {
     switch(process.env.DB_AUTH_METHOD) {
+        case 'azure-auth-token':
+            const { getAzureAuthToken } = require('./azure')
+            return await getAzureAuthToken()
         default:
             return process.env.DB_PASSWORD
     }
