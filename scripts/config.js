ea9d1bc320c6e3c4e8739a5bcba2f3beae7835af
IanR01 <ian@rieken.me>
2025-01-29 14:21:20 +0100

    fix: add missing DB_DIALECT env var to setup process, and set it to the new default 'mysql'

--- scripts/config.js
+++ scripts/config.js
@@ -23,6 +23,7 @@ async function setupEnvVars() {
   process.env.DB_USERNAME = process.env.DB_USERNAME || 'openstad';
   process.env.DB_PASSWORD = process.env.DB_PASSWORD || generateRandomToken({ length: 32 });
   process.env.DB_BASE_NAME = process.env.DB_BASE_NAME || ''
+  process.env.DB_DIALECT = process.env.DB_DIALECT || 'mysql'
   process.env.DB_REQUIRE_SSL = process.env.DB_REQUIRE_SSL || false;
   process.env.DB_AUTH_METHOD = process.env.DB_AUTH_METHOD || '';
 
@@ -70,7 +71,7 @@ async function setupEnvVars() {
   process.env.API_DB_USERNAME = process.env.API_DB_USERNAME || process.env.DB_USERNAME;
   process.env.API_DB_PASSWORD = process.env.API_DB_PASSWORD || process.env.DB_PASSWORD;
   process.env.API_DB_NAME = process.env.API_DB_NAME || ( process.env.DB_BASE_NAME ? process.env.DB_BASE_NAME + '-api' :  'openstad-api' );
-  process.env.API_DB_DIALECT = process.env.API_DB_DIALECT || process.env.DB_DIALECT || 'mariadb';
+  process.env.API_DB_DIALECT = process.env.API_DB_DIALECT || process.env.DB_DIALECT || 'mysql';
   process.env.API_DB_REQUIRE_SSL = process.env.API_DB_REQUIRE_SSL || process.env.DB_REQUIRE_SSL || false;
   process.env.API_DB_AUTH_METHOD = process.env.API_DB_AUTH_METHOD || process.env.DB_AUTH_METHOD || '';
 
@@ -104,6 +105,7 @@ async function setupEnvVars() {
   process.env.AUTH_DB_USERNAME = process.env.AUTH_DB_USERNAME || process.env.DB_USERNAME || '';
   process.env.AUTH_DB_PASSWORD = process.env.AUTH_DB_PASSWORD || process.env.DB_PASSWORD || '';
   process.env.AUTH_DB_NAME = process.env.AUTH_DB_NAME || ( process.env.DB_BASE_NAME ? process.env.DB_BASE_NAME + '-auth' :  'openstad-auth' );
+  process.env.AUTH_DB_DIALECT = process.env.AUTH_DB_DIALECT || process.env.DB_DIALECT || 'mysql';
   process.env.AUTH_DB_REQUIRE_SSL = process.env.AUTH_DB_REQUIRE_SSL || process.env.DB_REQUIRE_SSL || false;
   process.env.AUTH_DB_AUTH_METHOD = process.env.AUTH_DB_AUTH_METHOD || process.env.DB_AUTH_METHOD || '';
 
@@ -170,6 +172,7 @@ DB_HOST=${process.env.DB_HOST}
 DB_USERNAME=${process.env.DB_USERNAME}
 DB_PASSWORD=${process.env.DB_PASSWORD}
 DB_BASE_NAME=${process.env.DB_BASE_NAME}
+DB_DIALECT=${process.env.DB_DIALECT}
 DB_REQUIRE_SSL=${process.env.DB_REQUIRE_SSL}
 DB_AUTH_METHOD=${process.env.DB_AUTH_METHOD}
 
@@ -235,6 +238,7 @@ AUTH_DB_HOST=${process.env.AUTH_DB_HOST || 'openstad-mysql'}
 AUTH_DB_USER=${process.env.AUTH_DB_USERNAME}
 AUTH_DB_PASSWORD=${process.env.AUTH_DB_PASSWORD}
 AUTH_DB_NAME=${process.env.AUTH_DB_NAME}
+AUTH_DB_DIALECT=${process.env.AUTH_DB_DIALECT}
 AUTH_DB_REQUIRE_SSL=${process.env.AUTH_DB_REQUIRE_SSL}
 AUTH_DB_AUTH_METHOD=${process.env.AUTH_DB_AUTH_METHOD}
 
