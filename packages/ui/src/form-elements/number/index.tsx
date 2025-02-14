7ab255ee5e2236e40a993bc08f87b566207471ed
iandebruin98 <ian@draad.nl>
2025-02-14 16:15:04 +0100

    feat: Number formatting for the input

--- packages/ui/src/form-elements/number/index.tsx
+++ packages/ui/src/form-elements/number/index.tsx
@@ -36,54 +36,94 @@ const NumberInput: FC<NumberInputProps> = ({
     prepend,
     append,
 }) => {
-    const randomID = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    const [value, setValue] = useState(defaultValue);
+  const randomID = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  const [value, setValue] = useState(defaultValue);
+  const MAX_VALUE = 1_000_000_000_000;
 
-    useEffect(() => {
-        if (reset) {
-            reset(() => setValue(defaultValue));
-        }
-    }, [reset, defaultValue]);
+  useEffect(() => {
+    if (reset) {
+      reset(() => setValue(defaultValue));
+    }
+  }, [reset, defaultValue]);
 
-    return (
-        <FormField type="text">
-            {title && (
-                <Paragraph className="utrecht-form-field__label">
-                    <FormLabel htmlFor={randomID}>{title}</FormLabel>
-                </Paragraph>
-            )}
-            {description &&
-                <>
-                    <FormFieldDescription dangerouslySetInnerHTML={{__html: description}} />
-                    <Spacer size={.5} />
-                </>
+  const formatNumber = (num: string) => {
+    return num.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let inputValue = e.target.value.replace(/\D/g, "").replace(/^0+/, "");
+
+    if (inputValue === "") {
+      setValue("");
+      if (onChange) {
+        onChange({
+          name: fieldKey,
+          value: "",
+        });
+      }
+      return;
+    }
+
+    let numericValue = parseInt(inputValue, 10);
+    if (numericValue > MAX_VALUE) {
+      numericValue = MAX_VALUE;
+      inputValue = MAX_VALUE.toString();
+    }
+
+    setValue(format ? formatNumber(inputValue) : inputValue);
+
+    if (onChange) {
+      onChange({
+        name: fieldKey,
+        value: (numericValue).toString(),
+      });
+    }
+  };
+
+  return (
+    <FormField type="text">
+      {title && (
+        <Paragraph className="utrecht-form-field__label">
+          <FormLabel htmlFor={randomID}>{title}</FormLabel>
+        </Paragraph>
+      )}
+      {description && (
+        <>
+          <FormFieldDescription dangerouslySetInnerHTML={{__html: description}}/>
+          <Spacer size={0.5}/>
+        </>
+      )}
+
+      <div className={`utrecht-form-field__input`}>
+        {prepend && <span className="utrecht-form-field__prepend">{prepend}</span>}
+        <Textbox
+          id={randomID}
+          name={fieldKey}
+          type="text"
+          value={value}
+          onChange={handleChange}
+          onPaste={(e) => {
+            e.preventDefault();
+            let pastedData = e.clipboardData.getData("text").replace(/\D/g, "");
+            let numericValue = parseInt(pastedData, 10);
+            if (numericValue > MAX_VALUE) numericValue = MAX_VALUE;
+
+            setValue(format ? formatNumber(numericValue.toString()) : numericValue.toString());
+
+            if (onChange) {
+              onChange({
+                name: fieldKey,
+                value: (numericValue).toString(),
+              });
             }
-
-            <div className={`utrecht-form-field__input`}>
-                {prepend && <span className="utrecht-form-field__prepend">{prepend}</span>}
-                <Textbox
-                    id={randomID}
-                    name={fieldKey}
-                    type={'number'}
-                    value={value}
-                    onChange={(e) => {
-                        const newValue = parseFloat(e.target.value) < 0 ? "0" : e.target.value;
-
-                        setValue(newValue);
-                        if (onChange) {
-                            onChange({
-                                name: fieldKey,
-                                value: newValue,
-                            });
-                        }
-                    }}
-                    disabled={disabled}
-                    autoComplete={'off'}
-                />
-                {append && <span className="utrecht-form-field__append">{append}</span>}
-            </div>
-        </FormField>
-    );
+          }}
+          disabled={disabled}
+          autoComplete="off"
+        />
+        {append && <span className="utrecht-form-field__append">{append}</span>}
+      </div>
+    </FormField>
+  );
 };
 
 export default NumberInput;
