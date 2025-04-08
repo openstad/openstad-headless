907e6e94fcc2a60c1509bf4ae9ddde026e89b58a
Rudi van Hierden <rudi@draad.nl>
2025-04-08 13:13:11 +0200

    fix(admin): set `req` in handler

--- apps/admin-server/src/pages/api/auth-providers-enabled.ts
+++ apps/admin-server/src/pages/api/auth-providers-enabled.ts
@@ -1,10 +1,9 @@
-import { NextResponse } from 'next/server';
-import { NextApiResponse } from "next/types";
+import { NextApiRequest, NextApiResponse } from "next/types";
 
 /**
  * This handler returns the run-time environment variable ENABLE_AUTH_PROVIDERS
  * If we use NEXT_PUBLIC_ in this application, it will be defined in build time.
  */
-export default function handler(res: NextApiResponse) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.json({ authProvidersEnabled: process.env.ENABLE_AUTH_PROVIDERS });
 }
