d05b8606a87c08e677daa3a771d6bbe42ae173b7
Rudi van Hierden <rudi@draad.nl>
2025-04-28 11:26:39 +0200

    chore: logs for testing

--- apps/api-server/src/routes/auth/index.js
+++ apps/api-server/src/routes/auth/index.js
@@ -17,16 +17,21 @@ router.use( '/', require('./me') );
 router
   .use(async function (req, res, next) { // auth config
     let useAuth = req.query.useAuth || req.user.provider || ( req.user.idpUser && req.user.idpUser.provider );
+    console.log ('/auth/me route useAuth', useAuth);
     req.authConfig = await authSettings.config({ project: req.project, useAuth: useAuth })
+    console.log ('/auth/me route authConfig', req.authConfig, req.project);
     return next();
   })
   .use(async function (req, res, next) { // get adapter
     try {
       let adapter = req.authConfig.adapter || 'openstad';
+      console.log ('/auth/me route adapter', adapter);
       if (!adapters[adapter]) {
         // TODO: zo schrijf je geen dirname....
         adapters[adapter] = await authSettings.adapter({ authConfig: req.authConfig })
       }
+      
+      console.log ('/auth/me route adapters', adapters.length);
       return next();
     } catch(err) {
       return next(err);
@@ -34,6 +39,7 @@ router
   })
   .use(async function (req, res, next) { // use adapter
     let adapter = req.authConfig.adapter || 'openstad';
+    console.log ('/auth/me route use adapter', adapters[adapter] ?? 'adapter not found');
     return adapters[adapter].router(req, res, next)
   })
 
