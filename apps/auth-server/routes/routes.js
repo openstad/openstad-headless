b6fe750584371ec9fcfc130ddc4348a9834ace02
iandebruin98 <ian@draad.nl>
2025-05-16 15:28:36 +0200

    chore: Remove double function

--- apps/auth-server/routes/routes.js
+++ apps/auth-server/routes/routes.js
@@ -129,30 +129,6 @@ const rateLimiter = (req, res, next) => {
     next();
 };
 
-function rateLimiter({ limit = 100, windowMs = 60000 } = {}) {
-    return (req, res, next) => {
-        const ip = req.ip;
-        const now = Date.now();
-
-        if (!rateLimiters[ip]) {
-            rateLimiters[ip] = { count: 1, startTime: now };
-        } else {
-            const current = rateLimiters[ip];
-            if (now - current.startTime < windowMs) {
-                current.count++;
-            } else {
-                rateLimiters[ip] = { count: 1, startTime: now };
-            }
-        }
-
-        if (rateLimiters[ip].count > limit) {
-            return res.status(429).send('Too many requests. Please try again later.');
-        }
-
-        next();
-    };
-}
-
 module.exports = function (app) {
 
     app.use(function (req, res, next) {
