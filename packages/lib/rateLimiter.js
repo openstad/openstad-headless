1917e0941a26d13d4c2cefd886349af23ba5bbee
iandebruin98 <ian@draad.nl>
2025-06-25 08:44:50 +0200

    feat: Add rateLimit options

--- packages/lib/rateLimiter.js
+++ packages/lib/rateLimiter.js
@@ -4,6 +4,8 @@ function rateLimiter() {
     return rateLimit({
         windowMs: process.env.RATE_WINDOW_MS ? parseInt(process.env.RATE_WINDOW_MS, 10) : 60 * 1000,
         max: process.env.RATE_LIMIT ? parseInt(process.env.RATE_LIMIT, 10) : 100,
+        standardHeaders: true,
+        legacyHeaders: false
     });
 }
 
