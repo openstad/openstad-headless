35efd6569d86efaa4f2bb7a36f57d50884ac61f0
Rudi van Hierden <rudi@draad.nl>
2025-07-02 10:36:42 +0200

    fix(rate-limiter): skip private IPs
    This prevents the auth-server from (temporarily) blocking requests that
    are initiated by the API-server.

--- packages/lib/rateLimiter.js
+++ packages/lib/rateLimiter.js
@@ -1,11 +1,16 @@
 const rateLimit = require('express-rate-limit');
+const ip = require('ip');
 
 function rateLimiter() {
     return rateLimit({
         windowMs: process.env.RATE_WINDOW_MS ? parseInt(process.env.RATE_WINDOW_MS, 10) : 60 * 1000,
         max: process.env.RATE_LIMIT ? parseInt(process.env.RATE_LIMIT, 10) : 100,
         standardHeaders: true,
-        legacyHeaders: false
+        legacyHeaders: false,
+        skip: (req, res) => {
+            // Skip rate limiting for private IPs and localhost
+            return ip.isPrivate(req.ip) || req.ip === '::1';
+        }
     });
 }
 
