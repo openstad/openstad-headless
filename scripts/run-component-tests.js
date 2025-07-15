13a618bd639ae657d0121865e2e9ef36418a3750
Rudi van Hierden <rudi@draad.nl>
2025-07-15 09:59:02 +0200

    ci: update component test commands and environment variable reference

--- scripts/run-component-tests.js
+++ scripts/run-component-tests.js
@@ -5,30 +5,42 @@ const { execSync } = require('child_process');
 const packagesDir = path.resolve(__dirname, '../packages');
 const packageFolders = fs.readdirSync(packagesDir);
 
-const ciBuildId = process.env?.CI_BUILD_ID;
+const ciBuildId = process.env?.BUILD_ID;
 console.log(`CI Build ID: ${ciBuildId}`);
 
 for (const folder of packageFolders) {
   const packagePath = path.join(packagesDir, folder);
   const pkgJsonPath = path.join(packagePath, 'package.json');
+  const cypressPath = path.join(packagePath, 'cypress');
+  const cypressComponentPath = path.join(cypressPath, 'component');
 
   if (!fs.existsSync(pkgJsonPath)) continue;
 
-  let command = `test:component`
+  let command = `cypress run --component`
+  
+  // get current folder name
+  const group = folder.trim().replace(/\ /g, '-').toLowerCase();
+  
   
   if (process.env?.CYPRESS_RECORD_KEY) {
-    command = `test:component:record`;
+    command = `cypress run --component --record --group ${group}`;
+    
+    if (ciBuildId) {
+      command += ` --ci-build-id=${ciBuildId}`;
+    }
   }
   
-  const pkgJson = JSON.parse(fs.readFileSync(pkgJsonPath, 'utf8'));
-  if (pkgJson.scripts && pkgJson.scripts[command]) {
-    console.log(`\nRunning component tests in ${folder}...`);
-    try {
-      execSync(`npm run ${command} --ci-build-id=${ciBuildId}`, { stdio: 'inherit', cwd: packagePath });
-    } catch (err) {
-      console.error(`\nFailed running component test in ${folder}`);
-      // Exit with a non-zero code to indicate failure
-      process.exit(1);
-    }
+  // Check if the package has cypress component tests defined
+  if (!fs.existsSync(cypressPath) || !fs.existsSync(cypressComponentPath)) {
+    console.log(`\nSkipping ${folder} as it does not have component tests defined.`);
+    continue;
+  }
+  console.log(`\nRunning component tests in ${folder}...`);
+  try {
+    execSync(command, { stdio: 'inherit', cwd: packagePath });
+  } catch (err) {
+    console.error(`\nFailed running component test in ${folder}`);
+    // Exit with a non-zero code to indicate failure
+    process.exit(1);
   }
 }
