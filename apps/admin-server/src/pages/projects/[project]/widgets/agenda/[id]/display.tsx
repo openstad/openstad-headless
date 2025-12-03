d3841d7ae630bb1e8994d7d32a9ee0ee31d8a95d
iandebruin98 <ian@draad.nl>
2025-12-03 14:24:16 +0100

    feat: add toggle functionality to agenda component

--- apps/admin-server/src/pages/projects/[project]/widgets/agenda/[id]/display.tsx
+++ apps/admin-server/src/pages/projects/[project]/widgets/agenda/[id]/display.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button';
 import {
-  Form,
+  Form, FormControl,
   FormField,
   FormItem,
   FormLabel,
@@ -14,15 +14,25 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { AgendaWidgetProps } from '@openstad-headless/agenda/src/agenda';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
+import * as React from "react";
+import {Input} from "@/components/ui/input";
+import {useFieldDebounce} from "@/hooks/useFieldDebounce";
 
 const formSchema = z.object({
   displayTitle: z.boolean(),
+  displayToggle: z.boolean().optional(),
+  toggleDefaultClosed: z.boolean().optional(),
+  toggleShowText: z.string().optional(),
+  toggleHideText: z.string().optional(),
+  defaultClosedFromBreakpoint: z.enum(['not', '480', '640', '768', '1024']).optional()
 });
 
 export default function WidgetAgendaDisplay(
   props: AgendaWidgetProps & EditFieldProps<AgendaWidgetProps>
 ) {
   type FormData = z.infer<typeof formSchema>;
+  const { onFieldChange } = useFieldDebounce(props.onFieldChanged);
 
   async function onSubmit(values: FormData) {
     props.updateConfig({ ...props, ...values });
@@ -32,6 +42,11 @@ export default function WidgetAgendaDisplay(
     resolver: zodResolver<any>(formSchema),
     defaultValues: {
       displayTitle: props?.displayTitle || false,
+      displayToggle: props?.displayToggle || false,
+      toggleDefaultClosed: props?.toggleDefaultClosed || false,
+      toggleShowText: props?.toggleShowText || 'Lees meer',
+      toggleHideText: props?.toggleHideText || 'Lees minder',
+      defaultClosedFromBreakpoint: props?.defaultClosedFromBreakpoint || 'not',
     },
   });
 
@@ -54,6 +69,121 @@ export default function WidgetAgendaDisplay(
               </FormItem>
             )}
           />
+
+          <FormField
+            control={form.control}
+            name="displayToggle"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  Toon een knop om de agenda in/uit te klappen
+                </FormLabel>
+                {YesNoSelect(field, props)}
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          { form.watch('displayToggle') && (
+            <>
+              <FormField
+                control={form.control}
+                name="toggleDefaultClosed"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      Standaard de agenda ingeklapt weergeven
+                    </FormLabel>
+                    {YesNoSelect(field, props)}
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="toggleShowText"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      Tekst voor de knop om de agenda te tonen
+                    </FormLabel>
+                    <Input
+                      {...field}
+                      onChange={(e) => {
+                        field.onChange(e);
+                        onFieldChange(field.name, e.target.value);
+                      }}
+                    />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="toggleHideText"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      Tekst voor de knop om de agenda te verbergen
+                    </FormLabel>
+                    <Input
+                      {...field}
+                      onChange={(e) => {
+                        field.onChange(e);
+                        onFieldChange(field.name, e.target.value);
+                      }}
+                    />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              { !form.watch('toggleDefaultClosed') && (
+                <FormField
+                  control={form.control}
+                  name="defaultClosedFromBreakpoint"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>
+                        Breekpunt vanaf wanneer de agenda standaard ingeklapt wordt weergegeven
+                      </FormLabel>
+                      <Select
+                        onValueChange={(value) => {
+                          field.onChange(value);
+                          props.onFieldChanged(field.name, value);
+                        }}
+                        value={field.value}>
+                        <FormControl>
+                          <SelectTrigger>
+                            <SelectValue placeholder="Selecteer een optie" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          <SelectItem value="not">
+                            Nooit automatisch inklappen
+                          </SelectItem>
+                          <SelectItem value="480">
+                            <b>480 px</b>: Zeer kleine schermen en kleiner
+                          </SelectItem>
+                          <SelectItem value="640">
+                            <b>640 px</b>: Kleine schermen (mobiel) en kleiner
+                          </SelectItem>
+                          <SelectItem value="768">
+                            <b>768 px</b>: Middelgrote schermen (tablet) en kleiner
+                          </SelectItem>
+                          <SelectItem value="1024">
+                            <b>1024 px</b>: Grote schermen (laptop/desktop) en kleiner
+                          </SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </FormItem>
+                  )}
+                />
+              )}
+
+            </>
+          )}
+
 
           <Button className="w-fit col-span-full" type="submit">
             Opslaan
--- packages/agenda/src/agenda.tsx
+++ packages/agenda/src/agenda.tsx
@@ -7,65 +7,111 @@ import React from 'react';
 
 import "@utrecht/component-library-css";
 import "@utrecht/design-tokens/dist/root.css";
-import { Heading4, Heading3, Paragraph, LinkListLink, LinkList } from "@utrecht/component-library-react";
+import {
+  Heading4,
+  Heading3,
+  Paragraph,
+  LinkListLink,
+  LinkList
+} from "@utrecht/component-library-react";
+import { Accordion } from '@openstad-headless/ui/src/accordion';
 
 export type AgendaWidgetProps = BaseProps &
   ProjectSettingProps & {
-    projectId?: string;
-    resourceId?: string;
-  } & {
-    displayTitle?: boolean;
+  projectId?: string;
+  resourceId?: string;
+} & {
+  displayTitle?: boolean;
+  title?: string;
+  items?: Array<{
+    trigger: string;
     title?: string;
-    items?: Array<{
+    description: string;
+    active: boolean;
+    links?: Array<{
       trigger: string;
-      title?: string;
-      description: string;
-      active: boolean;
-      links?: Array<{
-        trigger: string;
-        title: string;
-        url: string;
-        openInNewWindow: boolean;
-      }>;
+      title: string;
+      url: string;
+      openInNewWindow: boolean;
     }>;
+  }>;
+  displayToggle?: boolean;
+  toggleDefaultClosed?: boolean;
+  toggleShowText?: string;
+  toggleHideText?: string;
+  defaultClosedFromBreakpoint?: 'not' | '480' | '640' | '768' | '1024';
+};
+
+function Agenda({
+  displayToggle = false,
+  toggleDefaultClosed = false,
+  toggleShowText = 'Lees meer',
+  toggleHideText = 'Lees minder',
+  defaultClosedFromBreakpoint = 'not',
+  ...props
+}: AgendaWidgetProps) {
+  const isClosedByDefault = () => {
+    if (toggleDefaultClosed) return true;
+    if (defaultClosedFromBreakpoint === 'not') return false;
+
+    const width = window.innerWidth;
+    const breakpoint = parseInt(defaultClosedFromBreakpoint);
+    if (width <= breakpoint) return true;
+    return false;
   };
 
-function Agenda(props: AgendaWidgetProps) {
+  const AgendaContent = (
+    <>
+      {props.displayTitle && props.title && <Heading3>{props.title}</Heading3>}
+      <section className="osc-agenda">
+        {props?.items && props?.items?.length > 0 && props.items
+          ?.sort((a, b) => parseInt(a.trigger) - parseInt(b.trigger))
+          .map((item) => (
+            <div
+              key={item.trigger}
+              className={`osc-agenda-item${item.active ? ' --active-item' : ''}`}
+              aria-current={item.active ? 'true' : undefined}
+            >
+              <div className="osc-date-circle"></div>
+              <div className="osc-agenda-content">
+
+                <Heading4>{item.title}</Heading4>
+                <Paragraph>{item.description}</Paragraph>
+                {item.links && item.links?.length > 0 && (
+                  <LinkList className="osc-agenda-list">
+                    {item.links?.map((link, index) => (
+                      <LinkListLink
+                        href={link.url}
+                        target={link.openInNewWindow ? '_blank' : '_self'}>
+                        {link.title}
+                      </LinkListLink>
+                    ))}
+                  </LinkList>
+                )}
+              </div>
+            </div>
+          ))
+        }
+      </section>
+    </>
+  );
+
   return (
     <div className="osc">
       <Spacer size={2} />
-      {props.displayTitle && props.title && <Heading3>{props.title}</Heading3>}
-      <section className="osc-agenda">
-        {props?.items &&
-          props?.items?.length > 0 &&
-          props.items
-            ?.sort((a, b) => parseInt(a.trigger) - parseInt(b.trigger))
-            .map((item) => (
-              <div
-                key={item.trigger}
-                className={`osc-agenda-item${item.active ? ' --active-item' : ''}`}
-                aria-current={item.active ? 'true' : undefined}
-              >
-                <div className="osc-date-circle"></div>
-                <div className="osc-agenda-content">
 
-                  <Heading4>{item.title}</Heading4>
-                  <Paragraph>{item.description}</Paragraph>
-                  {item.links && item.links?.length > 0 && (
-                    <LinkList className="osc-agenda-list">
-                      {item.links?.map((link, index) => (
-                        <LinkListLink
-                          href={link.url}
-                          target={link.openInNewWindow ? '_blank' : '_self'}>
-                          {link.title}
-                        </LinkListLink>
-                      ))}
-                    </LinkList>
-                  )}
-                </div>
-              </div>
-            ))}
-      </section>
+      {displayToggle ? (
+        <Accordion
+          content={ AgendaContent }
+          closeLabel={ toggleHideText }
+          openLabel={  toggleShowText }
+          headingLevel={ 3 }
+          expanded={ !isClosedByDefault() }
+        />
+      ) : (
+        AgendaContent
+      )}
+
     </div>
   );
 }
