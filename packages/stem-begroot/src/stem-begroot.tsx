30bb8028bf953208f9ef11043e336067e23a3ba2
iandebruin98 <ian@draad.nl>
2026-02-12 11:28:55 +0100

    fix: remove double import of hasRole

--- packages/stem-begroot/src/stem-begroot.tsx
+++ packages/stem-begroot/src/stem-begroot.tsx
@@ -4,7 +4,7 @@ import { Paginator, Spacer, Stepper } from '@openstad-headless/ui/src';
 //@ts-ignore D.type def missing, will disappear when datastore is ts
 import DataStore from '@openstad-headless/data-store/src';
 import { loadWidget } from '@openstad-headless/lib/load-widget';
-import { hasRole, getScopedSessionRandomSortSeed, canLikeResource, hasRole } from '@openstad-headless/lib';
+import { hasRole, getScopedSessionRandomSortSeed, canLikeResource } from '@openstad-headless/lib';
 import type { BaseProps, ProjectSettingProps } from '@openstad-headless/types';
 import { StemBegrootBudgetList } from './step-1/begroot-budget-list/stem-begroot-budget-list';
 import { StemBegrootResourceDetailDialog } from './step-1/begroot-detail-dialog/stem-begroot-detail-dialog';
