34db1c2f25fc29791bdf01b65a0412821126aaa1
iandebruin98 <ian@draad.nl>
2026-02-17 08:44:14 +0100

    chore: code formatting

--- packages/ui/types/src/infoImage/index.d.ts
+++ packages/ui/types/src/infoImage/index.d.ts
@@ -1,26 +1,17 @@
 import React from 'react';
-
 type ImageType = {
-  url: string;
-  imageAlt?: string;
-  imageDescription?: string;
+    url: string;
+    imageAlt?: string;
+    imageDescription?: string;
 };
 type InfoImageProps = {
-  imageFallback?: string;
-  imageAltFallback?: string;
-  imageDescriptionFallback?: string;
-  images?: ImageType[];
-  createImageSlider?: boolean;
-  addSpacer?: boolean;
-  imageClickable?: boolean;
+    imageFallback?: string;
+    imageAltFallback?: string;
+    imageDescriptionFallback?: string;
+    images?: ImageType[];
+    createImageSlider?: boolean;
+    addSpacer?: boolean;
+    imageClickable?: boolean;
 };
-declare const InfoImage: ({
-  imageFallback,
-  imageAltFallback,
-  imageDescriptionFallback,
-  images,
-  createImageSlider,
-  addSpacer,
-  imageClickable,
-}: InfoImageProps) => React.JSX.Element | React.JSX.Element[] | null;
+declare const InfoImage: ({ imageFallback, imageAltFallback, imageDescriptionFallback, images, createImageSlider, addSpacer, imageClickable, }: InfoImageProps) => React.JSX.Element | React.JSX.Element[] | null;
 export { InfoImage };
