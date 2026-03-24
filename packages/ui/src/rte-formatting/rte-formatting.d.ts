a88ad57ec8b44c8640b4e7479e05e6b2122a3550
oscarbakker <bakkeroscar@gmail.com>
2026-03-24 17:26:41 +0100

    Update ts files Remove dup const refreshComments Fix format resource js

--- packages/ui/src/rte-formatting/rte-formatting.d.ts
+++ packages/ui/src/rte-formatting/rte-formatting.d.ts
@@ -2,6 +2,8 @@ export interface RenderContentOptions {
   unwrapSingleRootDiv?: boolean;
 }
 
+export function unwrapSingleRootDiv(content: string): string;
+
 declare function RenderContent(
   content: any,
   options?: RenderContentOptions
