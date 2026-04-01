6e00e9b471f09e4f8de8585b4a0fe73a2db15ea8
Rudi van Hierden <rudi@draad.nl>
2026-04-01 14:13:38 +0200

    feat: replace CDN React loading with self-hosted namespaced runtime

--- apps/api-server/src/routes/widget/index.js
+++ apps/api-server/src/routes/widget/index.js
@@ -6,4 +6,19 @@ const cors = require('cors');
 // That will require websites to be allowlisted in the API server, but that's a good thing.
 router.use('/', cors(), require('./widget'));
 
+const path = require('path');
+// Serve the namespaced React runtime bundle
+router.use(
+  '/react-runtime.js',
+  express.static(
+    path.resolve(__dirname, '../../../dist/openstad-react-runtime.js'),
+    {
+      maxAge: '1d',
+      setHeaders: (res) => {
+        res.setHeader('Content-Type', 'application/javascript');
+      },
+    }
+  )
+);
+
 module.exports = router;
