9533e96451a0595a6898ed5387c292282a2b0108
iandebruin98 <ian@draad.nl>
2026-05-15 10:24:40 +0200

    fix: handle slug conflicts in implementParkOne method

--- apps/cms-server/modules/@apostrophecms/page/index.js
+++ apps/cms-server/modules/@apostrophecms/page/index.js
@@ -27,4 +27,58 @@ module.exports = {
       },
     },
   },
+  extendMethods(self) {
+    return {
+      async implementParkOne(_super, req, item) {
+        try {
+          return await _super(req, item);
+        } catch (e) {
+          if (
+            !self.apos.doc.isUniqueError(e) ||
+            !e.keyPattern?.slug ||
+            !e.keyPattern?.aposLocale
+          ) {
+            throw e;
+          }
+
+          const conflictSlug = e.keyValue?.slug;
+          const conflictLocale = e.keyValue?.aposLocale;
+
+          const parked = await self.apos.doc.db.findOne({
+            parkedId: item.parkedId,
+            aposLocale: conflictLocale,
+          });
+
+          const conflict = await self.apos.doc.db.findOne({
+            slug: conflictSlug,
+            aposLocale: conflictLocale,
+            _id: { $ne: parked?._id },
+          });
+
+          if (!conflict) {
+            throw e;
+          }
+
+          const dedupeSlug = `${conflictSlug}-duplicate-${conflict.aposDocId}`;
+
+          await self.apos.doc.db.updateOne(
+            { _id: conflict._id },
+            { $set: { slug: dedupeSlug } }
+          );
+
+          self.apos.util.warnDev(
+            `Resolved duplicate slug "${conflictSlug}" (locale: ${conflictLocale}): ` +
+              `renamed document ${conflict._id} to "${dedupeSlug}"`
+          );
+
+          try {
+            return await _super(req, item);
+          } catch (retryError) {
+            retryError.message = `Failed to park "${item.parkedId}" after resolving slug conflict: ${retryError.message}`;
+            throw retryError;
+          }
+        }
+      },
+    };
+  },
 };
