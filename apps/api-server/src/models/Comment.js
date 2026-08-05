3c2c0998c78f2e44407ad32c73997d03a9c3ebcf
Rudi van Hierden <rudi@draad.nl>
2026-08-05 12:59:38 +0200

    fix: include isStaffMember in reply serialization

--- apps/api-server/src/models/Comment.js
+++ apps/api-server/src/models/Comment.js
@@ -300,6 +300,7 @@ module.exports = function (db, sequelize, DataTypes) {
                 'hasUserDisliked',
                 'confirmationSent',
                 'yes',
+                'isStaffMember',
               ],
             },
           ],
